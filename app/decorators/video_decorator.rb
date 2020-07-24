c651ac0a7bc220f2e4193bbf412f33cf4500b5a2
Hampton Catlin <hcatlin@gmail.com>
2020-07-24 11:25:34 -0400

    Add in Decorators

---
+++ app/decorators/video_decorator.rb
@@ -0,0 +1,17 @@
+class VideoDecorator < Draper::Decorator
+  delegate_all
+
+  def thumbnail_url(width)
+
+  end
+
+  # Define presentation-specific methods here. Helpers are accessed through
+  # `helpers` (aka `h`). You can override attributes, for example:
+  #
+  #   def created_at
+  #     helpers.content_tag :span, class: 'time' do
+  #       object.created_at.strftime("%a %m/%d/%y")
+  #     end
+  #   end
+
+end
