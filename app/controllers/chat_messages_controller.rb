320fca074df10071ccb4ed8093dcc5e22912cd4a
Sirbuland <sirbuland.atlas@gmail.com>
2020-08-17 16:29:48 +0500

    completes flow for realtime messaging

--- app/controllers/chat_messages_controller.rb
+++ app/controllers/chat_messages_controller.rb
@@ -4,15 +4,19 @@ class ChatMessagesController < ApplicationController
   def create
     if user_signed_in?
       message = current_user.chat_messages.new(messages_params)
-      message.save!
-      ActionCable.server.broadcast "chat_room", 
-        { text: message.body, user_name: current_user.full_name }
+      if message.save
+        ActionCable.server.broadcast "chat_room", 
+          { text: message.body, user_name: current_user.full_name, video_id: message.video_id }
+        return render json: { success: true }
+      else
+        return render json: { success: false, error_messages: message.errors.full_messages }
+      end
     end
   end
 
   private
 
   def messages_params
-    params.require(:chat_message).permit(:body)
+    params.require(:chat_message).permit(:body, :video_id)
   end
 end
--- app/javascript/channels/chat_room_channel.js
+++ app/javascript/channels/chat_room_channel.js
@@ -12,6 +12,24 @@ consumer.subscriptions.create("ChatRoomChannel", {
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
-    console.log('data in chatRoomChannel', data)
+    const elementId = `video-chat-${data.video_id}`
+    const newMessageHtml = messageHtml(data.text, data.user_name)
+    const messageStream = document.getElementById(elementId)
+    if (messageHtml) {
+      messageStream.insertAdjacentHTML('beforeend', newMessageHtml)
+    }
   }
 });
+
+function messageHtml(message, name) {
+  return `
+    <div class="web--message">
+      <p class="web--message__name">
+        ${name}
+      </p>
+      <label class="web--message__label">
+        ${message}
+      </label>
+    </div>
+  `
+}
