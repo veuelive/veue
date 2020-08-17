a22da0a065d2c263bd3e35b39f1614517ed328a6
Sirbuland <sirbuland.atlas@gmail.com>
2020-08-17 16:42:26 +0500

    fixes after rebasing main branch

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,8 +1,2 @@
 // This is only here due to an annoying bug where ActiveAdmin includes sprockets
 //= link profile.png
-//= link profile-2.png
-//= link bell_06.png
-//= link dropdown-arrow_03.png
-//= link file-1.png
-//= link file-upload.png
-//= link man-img_03.png
