e9f185a716cacf8d5bfd62ca76bf8827666c1e88
Sirbuland <sirbuland.atlas@gmail.com>
2020-08-25 08:47:29 +0500

    corrects unauthorised spec

--- spec/controllers/chat_messages_controller_spec.rb
+++ spec/controllers/chat_messages_controller_spec.rb
@@ -2,7 +2,7 @@
 
 require "rails_helper"
 
-describe ChatMessagesController do
+describe ChatMessagesController, type: :controller do
   let(:user) { create(:user) }
   let(:video) { create(:video) }
   let(:chat_message_params) {
@@ -14,7 +14,7 @@ describe ChatMessagesController do
     }
   }
 
-  describe "create with authentication" do
+  describe "create chat message with authenticated user" do
     before do
       sign_in(user)
     end
@@ -26,19 +26,27 @@ describe ChatMessagesController do
       expect(user.chat_messages).not_to(be_empty)
     end
 
-    it "should not create message as body is not present" do
+    it "should not create message as 'body' is not present" do
       post :create, params: {chat_message: {video_id: video.id}}
 
       body = JSON.parse(response.body)
       expect(response).to have_http_status(200)
-      expect(body["error_messages"]).not_to(be_empty)
+      expect(body["error_messages"][0]).to eq("Body can't be blank")
+    end
+
+    it "should not create message as 'video_id' is not present" do
+      post :create, params: {chat_message: {body: Faker::Lorem.characters(number: 10)}}
+
+      body = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(body["error_messages"][0]).to eq("Video must exist")
     end
   end
 
   describe "create without authentication" do
     it "should not allow to create message" do
       post :create, params: chat_message_params
-      expect(response).to have_http_status(302)
+      expect(response).to redirect_to(new_user_session_path)
     end
   end
 end
