464d08472b9c3b31ad827365ec97c3a7c9fc61ba
Hampton Catlin <hcatlin@gmail.com>
2020-08-26 12:13:19 -0400

    Got the Streamer Veue working!

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,9 +20,10 @@ rescue ActiveRecord::PendingMigrationError => e
   exit(1)
 end
 RSpec.configure do |config|
-  config.include Devise::Test::IntegrationHelpers, type: :request
   config.include Devise::Test::IntegrationHelpers, type: :feature
+  config.include Devise::Test::IntegrationHelpers, type: :request
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
   config.include WebhookHelpers
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
