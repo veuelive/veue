1af107cb609b4364b3fa1bd367e167b503b7ee44
Sirbuland <sirbuland.atlas@gmail.com>
2020-08-26 21:22:31 +0500

    adds navbar and video streaming topbar

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,6 @@
 // This is only here due to an annoying bug where ActiveAdmin includes sprockets
 //= link profile.png
 //= link avatar-placeholder.png
+//= link main-logo.png
+//= link down-arrow.png
+//= link file-upload-icon.png
