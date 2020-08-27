d81ba98b826cac4becc4877bac302d15df818ae7
Sirbuland <sirbuland.atlas@gmail.com>
2020-08-27 21:49:08 +0500

    removes where condition from migration

--- db/migrate/20200827145649_generate_slugs_in_video.rb
+++ db/migrate/20200827145649_generate_slugs_in_video.rb
@@ -4,7 +4,7 @@ require "nanoid"
 
 class GenerateSlugsInVideo < ActiveRecord::Migration[6.0]
   def change
-    Video.where(slug: [nil, ""]).all.each do |v|
+    Video.all.each do |v|
       loop do
         v.slug = Nanoid.generate(size: 10)
         break unless Video.exists?(slug: v.slug)
