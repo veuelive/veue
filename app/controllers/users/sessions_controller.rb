ba20c61020ab427499631e0f833c96b45b6e2612
Sirbuland <sirbuland.atlas@gmail.com>
2020-08-31 13:56:08 +0500

    removes extra commented code

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -8,22 +8,5 @@ module Users
     # def new
     #   super
     # end
-
-    # POST /resource/sign_in
-    # def create
-    #   super
-    # end
-
-    # DELETE /resource/sign_out
-    # def destroy
-    #   super
-    # end
-
-    # protected
-
-    # If you have extra params to permit, append them to the sanitizer.
-    # def configure_sign_in_params
-    #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-    # end
   end
 end
