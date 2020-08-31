d7836dc05aa29ea2d3861de8209862971ab634fb
Hampton Catlin <hcatlin@gmail.com>
2020-08-31 13:22:39 -0400

    Demo version half working using WCS

--- app/javascript/controllers/streamer_view_controller.ts
+++ app/javascript/controllers/streamer_view_controller.ts
@@ -3,68 +3,88 @@ import FlashPhoner from "../flashphoner-webrtc-only";
 
 const SESSION_STATUS = FlashPhoner.constants.SESSION_STATUS;
 const STREAM_STATUS = FlashPhoner.constants.STREAM_STATUS;
+const url = "ws://splinter.veue.cloud:8080";
 
 export default class extends Controller {
-  static targets = ["videoContainer"];
-  readonly videoContainerTarget!: HTMLDivElement;
+  static targets = ["videoContainer", "debugCanvas"];
+  readonly videoContainerTarget!: HTMLVideoElement;
+  private debugCanvasTarget!: HTMLCanvasElement
   private streamKey: string;
+  private debugCanvasContext: CanvasRenderingContext2D;
+  private audioTrack: MediaStreamTrack;
 
   connect(): void {
-    const url = "ws://splinter.veue.cloud:8080";
     FlashPhoner.init({ url });
 
     this.streamKey = this.data.get("stream-key");
 
-    FlashPhoner.createSession({ urlServer: url })
-      .on(SESSION_STATUS.ESTABLISHED, (session) => {
-        //session connected, start streaming
-        console.log("We're streaming");
-        this.startStreaming(session);
-      })
-      .on(SESSION_STATUS.DISCONNECTED, () => {
-        this.onStopped();
-      })
-      .on(SESSION_STATUS.FAILED, () => {
-        this.onStopped();
-      });
+    this.debugCanvasContext = this.debugCanvasTarget.getContext("2d")
 
-    window.postMessage(
-      {
-        type: "veue",
-        action: "inject",
-        krang: this.data.get("krang-js-path"),
-        tox_path: this.data.get("tox-js-path"),
-      },
-      "*"
-    );
+    navigator.getUserMedia({audio: true, video: true}, (mediaStream) => {
+      this.videoContainerTarget.srcObject = mediaStream
+      this.audioTrack = mediaStream.getAudioTracks()[0]
+      // mediaStream.removeTrack(this.audioTrack)
+      this.videoContainerTarget.play()
+      this.timerCallback()
+      this.startStreaming()
+    }, (error) => {
+      console.error("Something went wrong", error)
+    })
+
+
   }
 
   private onStopped() {
     console.log("STOPPED");
   }
 
-  startStreaming(session): void {
-    session
-      .createStream({
-        name: this.streamKey + "-cam",
-        display: this.videoContainerTarget,
-        cacheLocalResources: true,
-        receiveVideo: false,
-        receiveAudio: false,
-      })
-      .on(STREAM_STATUS.PUBLISHING, (publishStream) => {
-        console.log(publishStream);
-      })
-      .on(STREAM_STATUS.UNPUBLISHED, function () {
-        // setStatus(STREAM_STATUS.UNPUBLISHED);
-        // //enable start button
-        // onStopped();
-      })
-      .on(STREAM_STATUS.FAILED, function (stream) {
-        // setStatus(STREAM_STATUS.FAILED, stream);
-        // //enable start button
-        // onStopped();
-      })
-      .publish();
+  startStreaming(): void {
+    FlashPhoner.createSession({ urlServer: url })
+        .on(SESSION_STATUS.ESTABLISHED, (session) => {
+          //session connected, start streaming
+          const mediaStream = this.debugCanvasTarget.captureStream(60) as MediaStream
+          mediaStream.addTrack(this.audioTrack)
+          session
+              .createStream({
+                name: this.streamKey,
+                display: document.createElement("video"),
+                constraints: {
+                  customStream: mediaStream
+                },
+                rtmpUrl: "rtmps://global-live.mux.com:443/app"
+              })
+              .on(STREAM_STATUS.PUBLISHING, (publishStream) => {
+                console.log(publishStream);
+              })
+              .on(STREAM_STATUS.UNPUBLISHED, function () {
+                // setStatus(STREAM_STATUS.UNPUBLISHED);
+                // //enable start button
+                // onStopped();
+              })
+              .on(STREAM_STATUS.FAILED, function (stream) {
+                // setStatus(STREAM_STATUS.FAILED, stream);
+                // //enable start button
+                // onStopped();
+              })
+              .publish();
+        })
+        .on(SESSION_STATUS.DISCONNECTED, () => {
+          this.onStopped();
+        })
+        .on(SESSION_STATUS.FAILED, () => {
+          this.onStopped();
+        });
+
+  }
+
+  timerCallback(): void {
+    this.computeFrame();
+    setTimeout(() => {
+      this.timerCallback();
+    }, 16); // roughly 60 frames per second
+  }
+
+  private computeFrame() {
+    this.debugCanvasContext.drawImage(this.videoContainerTarget, 200, 200)
   }
 }
--- app/javascript/controllers/veue_browser_controller.ts
+++ app/javascript/controllers/veue_browser_controller.ts
@@ -10,66 +10,7 @@ export default class extends Controller {
   krangMessageCallback: EventListenerOrEventListenerObject;
 
   connect(): void {
-    this.krangMessageCallback = (msg: MessageEvent) => {
-      const { type, event, secret } = msg.data;
-      switch (type) {
-        case "veue":
-          switch (event) {
-            // This comes after sending the activate
-            case "awaiting_command":
-              this.secret = secret;
-              this.sendKrangToDimensionX();
-          }
-          break;
-        case "krang":
-          this.paintFrame(msg.data);
-      }
-    };
-    // This sneaky bit of code uses a secret to send over some javascript
-    // that will get evaluated! :O
-    window.addEventListener("message", this.krangMessageCallback);
-    this.canvasCtx = this.canvasTarget.getContext("2d");
-    console.log("Ready to listen!");
+
   }
 
-  disconnect(): void {
-    window.removeEventListener("message", this.krangMessageCallback);
-  }
-
-  refresh(): void {
-    this.sendMessageToKrang("refresh");
-  }
-
-  addressChange(e: Event): void {
-    const target = e.target as HTMLInputElement;
-    this.sendMessageToKrang("go", target.value);
-  }
-
-  private sendKrangToDimensionX() {
-    this.sendMessageToKrang("awaken", {
-      krangPath: this.data.get("krang"),
-      technodromePath: this.data.get("technodrome"),
-    });
-  }
-
-  private sendMessageToKrang(event, payload?) {
-    const message = {
-      type: "veue",
-      event: event,
-      secret: this.secret,
-      payload: payload,
-    };
-    window.postMessage(message, window.location.origin);
-  }
-
-  private paintFrame(data: { frame: string; width: number; height: number }) {
-    const imageObj = new Image();
-    console.log(data);
-    imageObj.onload = () => {
-      this.canvasTarget.width = data.width;
-      this.canvasTarget.height = data.height;
-      this.canvasCtx.drawImage(imageObj, 0, 0);
-    };
-    imageObj.src = data.frame;
-  }
 }
