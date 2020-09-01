2f1f84d20bd8bba0381cc851ec722d224aa57e0c
Hampton Catlin <hcatlin@gmail.com>
2020-09-01 11:30:31 -0400

    Streaming works now! :O

--- deskie/index.js
+++ deskie/index.js
@@ -1,6 +1,6 @@
 'use strict';
 const path = require('path');
-const {app, BrowserWindow, Menu} = require('electron');
+const {app, BrowserWindow, Menu, BrowserView, ipcMain} = require('electron');
 /// const {autoUpdater} = require('electron-updater');
 const {is} = require('electron-util');
 const unhandled = require('electron-unhandled');
@@ -9,6 +9,11 @@ const contextMenu = require('electron-context-menu');
 const config = require('./config');
 const menu = require('./menu');
 const packageJson = require('./package.json');
+const { session } = require('electron')
+var ffmpegStatic = require('ffmpeg-static-electron');
+const child_process = require("child_process");
+const Buffer = require('buffer').Buffer;
+let ffmpeg;
 
 unhandled();
 debug();
@@ -30,16 +35,41 @@ app.setAppUserModelId(packageJson.build.appId);
 // Prevent window from being garbage collected
 let mainWindow;
 
+
 const createMainWindow = async () => {
 	const win = new BrowserWindow({
 		title: app.name,
 		show: false,
 		width: 2048,
-		height: 1200
+		height: 1200,
+		webPreferences: {
+			nodeIntegration: true
+		}
 	});
+
+	console.log("Hi!")
+
+	win.loadURL('http://localhost:3000/videos/broadcast').then(() => console.log("loaded"))
 
 	win.on('ready-to-show', () => {
 		win.show();
+
+		const view = new BrowserView()
+		win.setBrowserView(view)
+		view.setBounds({ x: 37, y: 137, width: 1200, height: 784 })
+		view.webContents.loadURL('https://jcrew.com')
+
+		let callback = () => {
+			view.webContents.capturePage()
+				.then((image) => {
+					console.log("Capture")
+					let dataUrl = image.toDataURL()
+					win.webContents.send("browser-frame", dataUrl)
+					setTimeout(callback, 100)
+				})
+		}
+
+		callback()
 	});
 
 	win.on('closed', () => {
@@ -48,8 +78,6 @@ const createMainWindow = async () => {
 		mainWindow = undefined;
 	});
 
-	await win.loadFile(path.join(__dirname, 'index.html'));
-
 	return win;
 };
 
@@ -57,6 +85,67 @@ const createMainWindow = async () => {
 if (!app.requestSingleInstanceLock()) {
 	app.quit();
 }
+
+
+ipcMain.on('stream', (event, data) => {
+	if(ffmpeg) {
+		console.log("Sending data to ffmpeg ", data.payload.length)
+		ffmpeg.stdin.write(data.payload);
+	} else {
+		console.log("Got data, but no good instance of ffmpeg")
+	}
+})
+
+ipcMain.on("start", (_, data) => {
+	const key = data.streamKey
+
+	const rtmpUrl = `rtmps://global-live.mux.com/app/${key}`;
+
+	console.log("Streaming to ", rtmpUrl)
+
+	ffmpeg = child_process.spawn('ffmpeg', [
+		'-i','-',
+
+		// video codec config: low latency, adaptive bitrate
+		'-c:v', 'libx264', '-preset', 'medium', '-tune', 'animation',
+
+		// audio codec config: sampling frequency (11025, 22050, 44100), bitrate 64 kbits
+		'-c:a', 'aac', '-strict', '-2', '-ar', '44100', '-b:a', '64k',
+
+		//force to overwrite
+		'-y',
+
+		// used for audio sync
+		'-use_wallclock_as_timestamps', '1',
+		'-async', '1',
+
+		//'-filter_complex', 'aresample=44100', // resample audio to 44100Hz, needed if input is not 44100
+		//'-strict', 'experimental',
+		'-bufsize', '1000',
+		'-f', 'flv',
+
+		rtmpUrl
+	]);
+
+	// Kill the WebSocket connection if ffmpeg dies.
+	ffmpeg.on('close', (code, signal) => {
+		console.log('FFmpeg child process closed, code ' + code + ', signal ' + signal);
+		ffmpeg = null
+	});
+
+	// Handle STDIN pipe errors by logging to the console.
+	// These errors most commonly occur when FFmpeg closes and there is still
+	// data to write.f If left unhandled, the server will crash.
+	ffmpeg.stdin.on('error', (e) => {
+		console.log('FFmpeg STDIN Error', e);
+	});
+
+	// FFmpeg outputs all of its messages to STDERR. Let's log them to the console.
+	ffmpeg.stderr.on('data', (data) => {
+		// console.log('FFmpeg STDERR:', data.toString());
+	});
+})
+
 
 app.on('second-instance', () => {
 	if (mainWindow) {
@@ -82,11 +171,17 @@ app.on('activate', () => {
 
 (async () => {
 	await app.whenReady();
+
+	session.defaultSession.cookies.set({
+		url: "http://localhost:3000/",
+		name: "_veue_session",
+		value: '0KeqvKZtOz3Q4HuGCK6eJxVWMeIkth9mBBialkCGpzusVtX%2BbwGGUPnwu%2Fj4hHjfAP1E4u8tWDYBsNSJ5d734bo2ofxEnvZ15QSck9tauB9saT0E%2Fzdw4FJu%2BR7U0%2FSyu4rCqN%2FuCd4LMvY2QeqQK9huF3FQfpFBSWCW12fbU3IsYqtwM78TbA83CVqO8PIjYc0HFzeZIW5w3iO%2BxdN4vhJA82H3ngIytKYwHQZnEyfO70m%2BAstfWVP%2Fb6OHsZeCKsQjj2PyjT3PcCmJklsE9jcnzuDhttujDtuVOqM9qhu93oGfJN6%2FsSMOALjAkOA1syVVDZ%2FFztdU7CiSA2xl8bLNy8xHedQexHHQNoqNmxxR179mE8cxkcGp48NQ7qm0PDh1mvI%3D--mgPfio%2BIIyvJikGj--u40tLGG733lIKGQN41zing%3D%3D'
+	})
+
 	Menu.setApplicationMenu(menu);
 	mainWindow = await createMainWindow();
 
-	const favoriteAnimal = config.get('favoriteAnimal');
+	// const favoriteAnimal = config.get('favoriteAnimal');
 
-
-	mainWindow.webContents.executeJavaScript(`document.querySelector('header p').textContent = 'Your favorite animal is ${favoriteAnimal}'`);
+	// mainWindow.webContents.executeJavaScript(`document.querySelector('header p').textContent = 'Your favorite animal is ${favoriteAnimal}'`);
 })();
