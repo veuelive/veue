661e9e44c89b7764f94ac11e5d028f58e5f76b50
Hampton Catlin <hcatlin@gmail.com>
2020-09-02 12:16:47 -0400

    Start of a detector, but I think I might try something else

--- app/javascript/controllers/streamer_view_controller.ts
+++ app/javascript/controllers/streamer_view_controller.ts
@@ -1,96 +1,188 @@
-import { Controller } from "stimulus";
+import {Controller} from "stimulus";
+import Bounds = chrome.system.display.Bounds;
+
+interface Rectangle {
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+}
 
 export default class extends Controller {
-  static targets = ["videoContainer", "debugCanvas"];
-  readonly videoContainerTarget!: HTMLVideoElement;
-  private debugCanvasTarget!: HTMLCanvasElement;
-  private streamKey: string;
-  private debugCanvasContext: CanvasRenderingContext2D;
-  private browserCanvas: HTMLCanvasElement;
-  private browserCanvasContext: CanvasRenderingContext2D;
-  private audioTrack: MediaStreamTrack;
-  private mediaRecorder: MediaRecorder;
-  private ipcRenderer: any;
+    static targets = ["debugCanvas"];
+    private debugCanvasTarget!: HTMLCanvasElement;
 
-  connect(): void {
-    // This should only ever run in the Electron App!
-    const { ipcRenderer } = eval("require('electron')");
-    this.ipcRenderer = ipcRenderer;
+    private webcamVideoElement: HTMLVideoElement;
+    private browserVideoElement: HTMLVideoElement;
 
-    this.streamKey = this.data.get("stream-key");
+    private debugCanvasContext: CanvasRenderingContext2D;
+    private audioTrack: MediaStreamTrack;
+    private mediaRecorder: MediaRecorder;
+    private ipcRenderer: any;
+    private browserDimensions: Rectangle;
 
-    this.debugCanvasContext = this.debugCanvasTarget.getContext("2d");
+    connect(): void {
+        // This should only ever run in the Electron App!
+        const {ipcRenderer} = eval("require('electron')");
+        this.ipcRenderer = ipcRenderer;
 
-    this.browserCanvas = document.createElement("canvas", {})
-    this.browserCanvasContext = this.browserCanvas.getContext("2d")
+        this.webcamVideoElement = document.createElement("video")
+        this.browserVideoElement = document.createElement("video")
 
-    navigator.getUserMedia(
-      { audio: true, video: true },
-      (mediaStream) => {
-        this.videoContainerTarget.srcObject = mediaStream;
-        this.audioTrack = mediaStream.getAudioTracks()[0];
-        // mediaStream.removeTrack(this.audioTrack)
-        this.videoContainerTarget.play();
-        this.timerCallback();
-        this.startStreaming();
-      },
-      (error) => {
-        console.error("Something went wrong", error);
-      }
-    );
+        document.body.setAttribute("style",  "display: none")
 
-    const capturer = eval("require('electron').desktopCapturer")
-    capturer.getSources({types: ["window", "screen"]}).then((sources) => console.log(sources))
-    ipcRenderer.on("browser-frame", (event, bitmap: Uint8Array, size) => {
-      // console.log(bitmap)
-      const imageData = new ImageData(new Uint8ClampedArray(bitmap), size.width, size.height)
-      if(size.width > 1280) {
-        this.browserCanvas.width = size.width
-        this.browserCanvas.height = size.height
-        this.browserCanvasContext.putImageData(imageData, 0, 0)
-        this.debugCanvasContext.drawImage(this.browserCanvas, 0, 0, 1280, 800)
-      }
+        this.debugCanvasContext = this.debugCanvasTarget.getContext("2d");
 
-    });
-  }
+        this.startWebcamCapture();
 
-  private onStopped() {
-    console.log("STOPPED");
-  }
+        // ipcRenderer.on("visible", async (_, windowName, dimensions: Rectangle, workArea: Rectangle) => {
+            this.startBrowserCapture()
+        // })
+    }
 
-  startStreaming(): void {
-    // Have to do this gross bit of code because typescript is convinced captureStream() doesn't exist
-    const canvas = this.debugCanvasTarget as any;
-    const mediaStream = canvas.captureStream(60) as MediaStream;
-    mediaStream.addTrack(this.audioTrack);
-    this.mediaRecorder = new MediaRecorder(mediaStream, {
-      mimeType: "video/webm",
-      videoBitsPerSecond: 3000000,
-    });
+    private startWebcamCapture() {
+        navigator.mediaDevices.getUserMedia(
+            {
+                audio: true, video: {
+                    width: 640,
+                    height: 480
+                }
+            }
+        ).then((mediaStream) => {
+            console.log("webcam", mediaStream)
+            this.webcamVideoElement.srcObject = mediaStream;
+            this.audioTrack = mediaStream.getAudioTracks()[0];
+            this.webcamVideoElement.play().then(() => {
+                this.timerCallback();
+            });
+        }).catch((error) => {
+            console.error("Something went wrong", error);
+        });
+    }
 
-    this.ipcRenderer.send("start", { streamKey: this.streamKey });
+    startStreaming(): void {
+        // Have to do this gross bit of code because typescript is convinced captureStream() doesn't exist
+        const canvas = this.debugCanvasTarget as any;
+        const mediaStream = canvas.captureStream(60) as MediaStream;
+        mediaStream.addTrack(this.audioTrack);
+        this.mediaRecorder = new MediaRecorder(mediaStream, {
+            mimeType: "video/webm",
+            videoBitsPerSecond: 3000000,
+        });
 
-    this.mediaRecorder.addEventListener("dataavailable", async (e) => {
-      const arrayBuffer = await e.data.arrayBuffer();
-      this.ipcRenderer.send("stream", { payload: new Uint8Array(arrayBuffer) });
-      console.log("Just sent data of size ", e.data.size, e.data.toString());
-    });
+        this.ipcRenderer.send("start", { streamKey: this.data.get("stream-key") });
 
-    this.mediaRecorder.addEventListener("stop", () => {
-      this.ipcRenderer.send("stop");
-    });
+        this.mediaRecorder.addEventListener("dataavailable", async (e) => {
+            const arrayBuffer = await e.data.arrayBuffer();
+            this.ipcRenderer.send("stream", {payload: new Uint8Array(arrayBuffer)});
+            console.log("Just sent data of size ", e.data.size, e.data.toString());
+        });
 
-    this.mediaRecorder.start(1000);
-  }
+        this.mediaRecorder.addEventListener("stop", () => {
+            this.ipcRenderer.send("stop");
+        });
 
-  timerCallback(): void {
-    this.computeFrame();
-    setTimeout(() => {
-      this.timerCallback();
-    }, 16); // roughly 60 frames per second
-  }
+        this.mediaRecorder.start(1000);
+    }
 
-  private computeFrame() {
-    this.debugCanvasContext.drawImage(this.videoContainerTarget, 0, 784);
-  }
+    private async startBrowserCapture() {
+        const capturer = eval("require('electron').desktopCapturer")
+        let source
+
+        while(!source) {
+            const sources = await capturer.getSources({types: ["window"]})
+            source = sources.find((source) => source.name === "Veue")
+        }
+
+        const mediaStream = await navigator.mediaDevices.getUserMedia({
+            audio: false,
+            video: {
+                mandatory: {
+                    chromeMediaSource: 'desktop',
+                    chromeMediaSourceId: source.id,
+                }
+            } as Record<string, unknown>
+        });
+
+        this.browserVideoElement.srcObject = mediaStream
+        document.body.appendChild((this.browserVideoElement))
+        await this.browserVideoElement.play()
+
+        this.detectBrowserArea();
+
+        console.log("browser", mediaStream)
+    }
+
+    private detectBrowserArea() {
+        const canvas = document.createElement("canvas")
+        canvas.width = this.browserVideoElement.videoWidth
+        canvas.height = this.browserVideoElement.videoHeight
+        const ctx = canvas.getContext("2d")
+        ctx.drawImage(this.browserVideoElement, 0, 0, this.browserVideoElement.videoHeight, this.browserVideoElement.videoWidth)
+        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+
+        const l = imageData.data.length / 4;
+
+        const inRange = (color, min, max) => {
+            return color > min && color < max
+        }
+
+        let topX = -1
+        let topY = -1
+        let bottomX = 0
+        let bottomY = 0
+
+        for (let i = 0; i < l; i++) {
+            const r = imageData.data[i * 4 + 0];
+            const g = imageData.data[i * 4 + 1];
+            const b = imageData.data[i * 4 + 2];
+
+
+            if(inRange(r, 0, 55) && inRange(g, 100, 255) && inRange(b, 0, 55)) {
+                const x = Math.floor(i / imageData.width)
+                const y = i % imageData.width
+                if(x > bottomX) {
+                    bottomX = x
+                }
+                if(y > bottomY) {
+                    bottomY = y
+                }
+                if(topX == -1) {
+                    topX = x
+                }
+                if(topY == -1) {
+                    topY = y
+                }
+            }
+        }
+
+        const width = bottomX - topX
+        const height = bottomY - topY
+
+        console.log("DONE!", width, height)
+
+    }
+
+    timerCallback(): void {
+        this.computeFrame();
+        setTimeout(() => {
+            this.timerCallback();
+        }, 16); // roughly 60 frames per second
+    }
+
+    private computeFrame() {
+        this.debugCanvasContext.drawImage(this.webcamVideoElement, 0, 800);
+
+        if (this.browserDimensions) {
+            this.debugCanvasContext.drawImage(this.browserVideoElement,
+                this.browserDimensions.x,
+                this.browserDimensions.y,
+                1280,
+                800,
+                0,
+                0,
+                1280,
+                800)
+        }
+    }
 }
