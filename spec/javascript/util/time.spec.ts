6169a7708e11977ed40a2b0f4ee562d96f005f72
Hampton Catlin <hcatlin@gmail.com>
2020-09-25 12:25:11 -0400

    Create time helpers

---
+++ spec/javascript/util/time.spec.ts
@@ -0,0 +1,10 @@
+import {displayTime} from "../../../app/javascript/util/time";
+
+describe("displayTime", () => {
+    it("should display various times", () => {
+        expect(displayTime(1)).toEqual("00:00:01")
+        expect(displayTime(82)).toEqual("00:01:22")
+        expect(displayTime(1982)).toEqual("00:33:22")
+        expect(displayTime(68882)).toEqual("19:08:02")
+    });
+});
