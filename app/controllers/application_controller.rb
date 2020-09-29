be1c89bfed83edb7bccebd851839406ca3fa694e
Sirbuland <sirbuland.atlas@gmail.com>
2020-09-29 18:02:40 +0500

    design tweeks to make video bigger

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,10 @@ class ApplicationController < ActionController::Base
   include HttpAuthConcern
   include AuthenticationConcern
   include IpcMockConcern if Rails.env.test?
+
+  protected
+
+  def skip_navbar
+    @skip_nav = true    
+  end
 end
