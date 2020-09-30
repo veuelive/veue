6f8e01d3d39bd38384103afd468b9fab86d711ce
Hampton Catlin <hcatlin@gmail.com>
2020-09-30 17:48:15 -0400

    SO MUCH!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,4 @@ class ApplicationController < ActionController::Base
   include HttpAuthConcern
   include AuthenticationConcern
   include IpcMockConcern unless Rails.env.production?
-
-  protected
-
-  def skip_navbar
-    @skip_nav = true
-  end
 end
