58cd34115b26b50ecd5e27902547e92318250335
Hampton Catlin <hcatlin@gmail.com>
2020-10-01 17:01:18 -0400

    add return type

--- app/javascript/controllers/pip_controller.ts
+++ app/javascript/controllers/pip_controller.ts
@@ -3,7 +3,8 @@ import { Controller } from "stimulus";
 export default class extends Controller {
   static targets = ["canvas"];
 
-  connect() {
+  connect(): void {
+    super.connect();
     this.element.setAttribute(
       "style",
       "position: absolute; bottom: 2vw; right: 2vw;"
