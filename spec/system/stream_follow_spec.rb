61af0dec5da28da38ac2416fbabae1e99a07c5eb
Sirbuland <sirbuland.atlas@gmail.com>
2020-10-03 07:57:35 +0500

    adds request specs

--- spec/system/stream_follow_spec.rb
+++ spec/system/stream_follow_spec.rb
@@ -21,39 +21,45 @@ RSpec.describe "Follow live streamer" do
     end
   end
 
-  describe "user logged in" do
+  describe "user logged in and follow record is not present" do
     before do
       login_as(follower)
       visit video_path(video)
     end
 
     it "should create a follow record in db" do
-      visit video_path(video)
       find(".follow-btn").click
       page.refresh
       expect(Follow.count).to eq(1)
     end
+  end
+
+  describe "user logged in and follow record present already" do
+    before do
+      login_as(follower)
+      @follow = Follow.create!(
+        streamer_user_id: streamer.to_param,
+        follower_user_id: follower.to_param,
+      )
+    end
 
     it "should update the unfollowed_at time in follow record" do
-      follow = Follow.create!(streamer_user_id: streamer.to_param, follower_user_id: follower.to_param)
       visit video_path(video)
       find(".follow-btn").click
       page.refresh
-      follow.reload
-      expect(follow.unfollowed_at).to_not eq(nil)
+      @follow.reload
+      expect(@follow.unfollowed_at).to_not eq(nil)
     end
 
     it "should set the unfollowed_at time to nil" do
-      follow = Follow.create!(
-        streamer_user_id: streamer.to_param,
-        follower_user_id: follower.to_param,
+      @follow.update!(
         unfollowed_at: Time.current,
       )
       visit video_path(video)
       find(".follow-btn").click
       page.refresh
-      follow.reload
-      expect(follow.unfollowed_at).to eq(nil)
+      @follow.reload
+      expect(@follow.unfollowed_at).to eq(nil)
     end
   end
 end
