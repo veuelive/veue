a64fdeaa8bab3bdc3718807512649ef38e6f48a6
Hampton Catlin <hcatlin@gmail.com>
2020-10-04 16:43:24 -0400

    use ms instead of centiseconds

--- spec/javascript/util/timecode.spec.ts
+++ spec/javascript/util/timecode.spec.ts
@@ -3,19 +3,21 @@ import Timecode from "../../../app/javascript/util/timecode";
 describe("timecodes", () => {
   describe("subBit color encoding", () => {
     it("should encode digits", () => {
-      expect(Timecode.encodedDigitToColor("0")).toEqual("000000");
-      expect(Timecode.encodedDigitToColor("1")).toEqual("0000FF");
-      expect(Timecode.encodedDigitToColor("2")).toEqual("00FF00");
-      expect(Timecode.encodedDigitToColor("3")).toEqual("00FFFF");
-      expect(Timecode.encodedDigitToColor("4")).toEqual("FF0000");
-      expect(Timecode.encodedDigitToColor("5")).toEqual("FF00FF");
-      expect(Timecode.encodedDigitToColor("6")).toEqual("FFFF00");
-      expect(Timecode.encodedDigitToColor("7")).toEqual("FFFFFF");
+      expect(Timecode.encodedDigitToColor("0")).toEqual("#000000");
+      expect(Timecode.encodedDigitToColor("1")).toEqual("#0000FF");
+      expect(Timecode.encodedDigitToColor("2")).toEqual("#00FF00");
+      expect(Timecode.encodedDigitToColor("3")).toEqual("#00FFFF");
+      expect(Timecode.encodedDigitToColor("4")).toEqual("#FF0000");
+      expect(Timecode.encodedDigitToColor("5")).toEqual("#FF00FF");
+      expect(Timecode.encodedDigitToColor("6")).toEqual("#FFFF00");
+      expect(Timecode.encodedDigitToColor("7")).toEqual("#FFFFFF");
     });
   });
 
   it("should do full encoding to colors", () => {
-    expect(Timecode.numberToColors(0)).toEqual(new Array(8).fill("000000"));
+    expect(Timecode.numberToColors(0)).toEqual(
+      new Array(Timecode.codeCount).fill("#000000")
+    );
   });
 
   it("can decode a color sequence", () => {
