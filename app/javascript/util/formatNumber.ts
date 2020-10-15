3e505145a94ab49453a165a2435b0e6d74e567d6
Sirbuland <sirbuland.atlas@gmail.com>
2020-10-15 18:58:25 +0500

    redescribe ranges

--- app/javascript/util/formatNumber.ts
+++ app/javascript/util/formatNumber.ts
@@ -1,8 +1,6 @@
 const ranges = [
-  { divider: 1e18, suffix: "E" },
-  { divider: 1e15, suffix: "P" },
   { divider: 1e12, suffix: "T" },
-  { divider: 1e9, suffix: "G" },
+  { divider: 1e9, suffix: "B" },
   { divider: 1e6, suffix: "M" },
   { divider: 1e3, suffix: "K" },
 ];
