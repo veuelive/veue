c65dc4d1a574b343540ee12fd146b5bc4096bb1b
Hampton Catlin <hcatlin@gmail.com>
2020-10-22 18:21:53 -0400

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root "velvet_rope#index"
+  if Rails.env.production?
+    root "velvet_rope#index"
+  else
+    root "videos#index"
+  end
 
   resources :videos, only: %i[index show new] do
     collection do
