72004b67b18be13257d3ad1e47211c48655d42a1
Jason Fleetwood-Boldt <jason.fb@datatravels.com>
2020-11-09 10:52:03 -0500

    [VEUE-209] updates all videos new visibility setting to 'public'

---
+++ db/migrate/20201109154733_add_default_visibility_to_videos.rb
@@ -0,0 +1,6 @@
+class AddDefaultVisibilityToVideos < ActiveRecord::Migration[6.0]
+  def up
+    execute `ALTER TABLE videos ALTER COLUMN visibility SET DEFAULT 'public'`
+    Video.update_all(:visibility => 'public')
+  end
+end
