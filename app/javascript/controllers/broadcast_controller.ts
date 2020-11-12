b8ddcb0cb187380c48da21878c49248b3c4fe585
Jason Fleetwood-Boldt <jason.fb@datatravels.com>
2020-11-12 11:16:06 -0500

    [VEUE-192] proof-of-concept for copy-to-clipboard (note several defects)

--- app/javascript/controllers/broadcast_controller.ts
+++ app/javascript/controllers/broadcast_controller.ts
@@ -112,7 +112,18 @@ export default class extends Controller {
     this.state = "finished";
     this.streamCapturer.stop();
   }
-
+  copyToClipboard(): void {
+    const vid_path = window.location.href.replace("broadcasts/", "videos/");
+    const copy_el = document.createElement("textarea");
+    copy_el.value = vid_path;
+    copy_el.setAttribute("readonly", "");
+    copy_el.style.position = "absolute";
+    copy_el.style.left = "-9999px";
+    document.body.appendChild(copy_el);
+    copy_el.select();
+    document.execCommand("copy");
+    document.body.removeChild(copy_el);
+  }
   pinPage(): void {
     const url = document
       .querySelector("input[data-target='broadcast--browser.addressBar']")
