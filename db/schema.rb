e473ead29e04358bccb99e1bf93cec2e3ea8833f
ParamagicDev <konnor5456@gmail.com>
2020-11-13 04:02:25 -0500

    chore: update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_09_154733) do
+ActiveRecord::Schema.define(version: 2020_11_13_013045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
