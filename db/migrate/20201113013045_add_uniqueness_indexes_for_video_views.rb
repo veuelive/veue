627fabe0dbb645129e58f302b66ea4ef98c6fe3f
ParamagicDev <konnor5456@gmail.com>
2020-11-13 04:03:04 -0500

    chore: add a migration for video views

---
+++ db/migrate/20201113013045_add_uniqueness_indexes_for_video_views.rb
@@ -0,0 +1,29 @@
+class AddUniquenessIndexesForVideoViews < ActiveRecord::Migration[6.0]
+  def change
+    remove_duplicates
+    add_index :video_views, :user_id, unique: true, where: "user_id IS NOT NULL"
+    add_index :video_views, :details, unique: true, where: "user_id IS NULL"
+  end
+
+  def remove_duplicates
+    remove_duplicate_details_on_nil_users
+    remove_duplicate_users
+  end
+
+  # Remove duplicate VideoView.details for nil users
+  def remove_duplicate_details_on_nil_users
+    VideoView.where("user_id IS NULL")
+      .group_by { |model| [model.video_id, model.details] }.values.each do |duplicate|
+        duplicate.shift # Pull out first record
+        duplicate.each(&:destroy) # Destroy all following records
+      end
+  end
+
+  def remove_duplicate_users
+    VideoView.where("user_id IS NOT NULL")
+      .group_by { |model| [model.video_id, model.user_id] }.values.each do |duplicate|
+        duplicate.shift
+        duplicate.each(&:destroy)
+      end
+  end
+end
