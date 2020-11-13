362512bd624e8c16aa773bd2b0b16fead22548ea
ParamagicDev <konnor5456@gmail.com>
2020-11-13 04:01:49 -0500

    feat: add uniqueness validator on video views

--- app/models/video_view.rb
+++ app/models/video_view.rb
@@ -3,4 +3,16 @@
 class VideoView < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :video, counter_cache: true
+
+  # Only 1 user allowed to view per video
+  validates :user_id,
+            uniqueness: {
+              allow_nil: true,
+              scope: :video_id
+            }
+
+  # # If its a "NULL" user, only 1 set of unique details allowed.
+  validates :details,
+            if: -> { user_id.nil? },
+            uniqueness: {scope: :video_id}
 end
