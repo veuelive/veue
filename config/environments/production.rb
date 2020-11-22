843585577336c6963cf32abdeb5ead840615f5cf
Hampton Catlin <hcatlin@gmail.com>
2020-11-22 10:59:03 -0500

    [VEUE-258] LogDNA Configuration
    Set up LogDNA.com as our logger for Veue.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,9 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
+  if ENV["LOG_DNA_KEY"].present?
+    config.logger = Logdna::Ruby.new(ENV["LOG_DNA_KEY"])
+  elsif ENV["RAILS_LOG_TO_STDOUT"].present?
     logger = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
