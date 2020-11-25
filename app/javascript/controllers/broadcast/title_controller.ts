4ab5ca9714391271651308bb4d26c2849a06b41d
ParamagicDev <konnor5456@gmail.com>
2020-11-25 09:52:48 -0500

    [VEUE-163] feat: Add broadcast title update

--- app/javascript/controllers/broadcast/title_controller.ts
+++ app/javascript/controllers/broadcast/title_controller.ts
@@ -3,21 +3,59 @@ import { getCurrentVideoId } from "helpers/event/live_event_manager";
 import { putForm } from "util/fetch";
 
 export default class extends Controller {
-  async doSubmit(event: KeyboardEvent): Promise<void> {
-    if (event.key != "Enter" || event.shiftKey) {
-      return;
-    }
+  static targets = ["input", "submit"];
 
-    if (!("currentTarget" in event)) {
+  private inputTarget!: HTMLInputElement;
+  private submitTarget!: HTMLButtonElement;
+  private timeoutId!: number;
+
+  initialize(): void {
+    this.inputTarget.dataset.dbValue = this.inputTarget.value;
+  }
+
+  async doSubmit(event: MouseEvent | KeyboardEvent): Promise<void> {
+    // "key" in event tells us we're dealing with a keyboard event
+    if ("key" in event && event.key != "Enter") {
       return;
     }
 
     event.preventDefault();
-    const input = event.currentTarget as HTMLInputElement;
+    const input = this.inputTarget;
     const title = input.value;
     const videoId = getCurrentVideoId();
-    input.disabled = true;
+
+    this.disableTargets();
     await putForm(`/videos/${videoId}`, { title });
-    input.disabled = false;
+    this.inputTarget.dataset.dbValue = title;
+    this.enableTargets();
+  }
+
+  /**
+   * If a user clicks off the text box, revert to previous title,
+   * Use a timeout so people can tab and not have the values revert
+   */
+  handleBlur(): void {
+    this.timeoutId = window.setTimeout(() => {
+      if (this.inputTarget.dataset.dbValue) {
+        this.inputTarget.value = this.inputTarget.dataset.dbValue;
+      } else {
+        this.inputTarget.value = "";
+      }
+    }, 2000);
+  }
+
+  /** We dont want to clear input while a user is focused on submit */
+  handleFocus(): void {
+    window.clearTimeout(this.timeoutId);
+  }
+
+  private disableTargets() {
+    this.inputTarget.disabled = true;
+    this.submitTarget.disabled = true;
+  }
+
+  private enableTargets() {
+    this.inputTarget.disabled = false;
+    this.submitTarget.disabled = false;
   }
 }
