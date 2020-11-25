80c65caa9a4cb65eb0803e6bc96e8ddb18595030
ParamagicDev <konnor5456@gmail.com>
2020-11-25 18:48:20 -0500

    feat: add ability to update title of a stream in progress

--- app/javascript/controllers/broadcast/title_controller.ts
+++ app/javascript/controllers/broadcast/title_controller.ts
@@ -2,11 +2,23 @@ import { Controller } from "stimulus";
 import { secureFormFetch } from "util/fetch";
 
 export default class extends Controller {
-  doSubmit(event: Event): void {
+  async doSubmit(event: Event): Promise<void> {
     event.preventDefault();
     const target = event.target as HTMLElement;
     const form = target.closest("form");
-    const formData = new FormData(form);
-    secureFormFetch(form.action, form.method, formData);
+    await secureFormFetch(form.action, form.method, form);
+    this.enableButton(form);
+  }
+
+  /**
+   * Re-enable a button after disabling it via UJS
+   */
+  enableButton(form: HTMLFormElement): void {
+    const disabledBtn = form.querySelector(
+      "input[data-disable-with][disabled]"
+    ) as HTMLInputElement;
+    if (disabledBtn.disabled == true) {
+      disabledBtn.disabled = false;
+    }
   }
 }
