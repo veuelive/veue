14e1e6a271226292961166a8c3a8fd5fdee0c5aa
Sirbuland <sirbuland.atlas@gmail.com>
2020-11-26 12:17:14 +0500

    [VEUE-227] fixes minor styles

--- app/javascript/controllers/user_reaction_controller.ts
+++ app/javascript/controllers/user_reaction_controller.ts
@@ -2,7 +2,7 @@ import { Controller } from "stimulus";
 import { post } from "util/fetch";
 
 export default class extends Controller {
-  sendReaction(event: Event): void {
+  sendReaction(): void {
     post("./reaction").then(() =>
       console.log("User Like/Reaction Event Generated")
     );
