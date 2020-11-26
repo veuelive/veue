6a2479c7075c0b4a7c7385dc9083291a01f5bfee
Sirbuland <sirbuland.atlas@gmail.com>
2020-11-26 21:47:28 +0500

    [VEUE-269] adds 5s delay in next click using enable/disable state

--- app/javascript/controllers/user_reaction_controller.ts
+++ app/javascript/controllers/user_reaction_controller.ts
@@ -2,9 +2,26 @@ import { Controller } from "stimulus";
 import { post } from "util/fetch";
 
 export default class extends Controller {
+  connect(): void {
+    this.state = "enabled";
+  }
+
   sendReaction(): void {
-    post("./reaction").then(() =>
-      console.log("User Like/Reaction Event Generated")
-    );
+    if (this.state === "enabled") {
+      post("./reaction").then(() =>
+        console.log("User Like/Reaction Event Generated")
+      );
+      this.state = "disabled";
+    }
+  }
+
+  set state(state: string) {
+    this.data.set("state", state);
+    if (this.state === "disabled")
+      setTimeout(() => (this.state = "enabled"), 5000);
+  }
+
+  get state(): string {
+    return this.data.get("state");
   }
 }
