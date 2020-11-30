5d0893acdbc5c6c59a7b39f2db0ec5fde1b944d1
Jason Fleetwood-Boldt <jason.fb@datatravels.com>
2020-11-30 18:51:10 -0500

    [VEUE-239] wires up the drop events but this is still having trouble with the TS controller implements drag & drop on PIP with targets attached to audience-view controller

--- app/javascript/controllers/audience/pip_controller.ts
+++ app/javascript/controllers/audience/pip_controller.ts
@@ -1,13 +1,28 @@
+import { polyfill } from "mobile-drag-drop";
+
+// optional import of scroll behaviour
+import { scrollBehaviourDragImageTranslateOverride } from "mobile-drag-drop/scroll-behaviour";
+
+// options are optional ;)
+polyfill({
+  // use this to make use of the scroll behaviour
+  dragImageTranslateOverride: scrollBehaviourDragImageTranslateOverride,
+});
+
 import { Controller } from "stimulus";
 
-export default class extends Controller {
+export default class PipController extends Controller {
   static targets = ["canvas"];
-
   connect(): void {
+    this.element.classList.add("bottom-right");
+    this.element.draggable = true;
+    this.element.ondragstart = this.onDragStart.bind(this);
+
     super.connect();
-    this.element.setAttribute(
-      "style",
-      "position: absolute; bottom: 2vw; right: 2vw;"
-    );
+  }
+
+  onDragStart(event): void {
+    event.effectAllowed = "move";
+    event.dropEffect = "none";
   }
 }
---
+++ app/javascript/controllers/audience/pip_quadrants_controller.ts
@@ -0,0 +1,18 @@
+import { Controller } from "stimulus";
+
+export default class PipQuadrantsController extends Controller {
+  connect(): void {
+    this.element.ondragenter = this.onDragEnter.bind(this);
+    this.element.ondragleave = this.onDragLeave.bind(this);
+    this.element.ondragover = this.onDragOver.bind(this);
+    // ondrop is handled in audience-view
+    super.connect();
+  }
+
+  onDragLeave(event): void {}
+  onDragEnter(event): void {}
+
+  onDragOver(event): void {
+    event.preventDefault();
+  }
+}
