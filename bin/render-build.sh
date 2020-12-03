84a1cbec38c0e40aa0739643b41549d124050e84
Hampton Catlin <hcatlin@gmail.com>
2020-12-03 16:47:10 -0500

    [VEUE-290] SideKiq Pro!
    This installs a new web UI that we can use to monitor jobs and queues in prod.
    
    Devs will need to run a command in order to get this working locally!

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,8 @@
 # exit on error
 set -o errexit
 
+bundle config --local gems.contribsys.com ab21b078:c8a6ea8f
+
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
+require 'sidekiq/web'
+require 'admin_constraint'
 
 Rails.application.routes.draw do
   if ENV["VELVET_ROPE"]
@@ -61,4 +63,16 @@ Rails.application.routes.draw do
   post "/mux/webhook", to: "mux_webhooks#index"
 
   ActiveAdmin.routes(self)
+
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    # Protect against timing attacks:
+    # - See https://codahale.com/a-lesson-in-timing-attacks/
+    # - See https://thisdata.com/blog/timing-attacks-against-string-comparison/
+    # - Use & (do not use &&) so that it doesn't short circuit.
+    # - Use digests to stop length information leaking (see also ActiveSupport::SecurityUtils.variable_size_secure_compare)
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
+  end if Rails.env.production?
+
+  mount Sidekiq::Web => '/_/sidekiq'
 end
