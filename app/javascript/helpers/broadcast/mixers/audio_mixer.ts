b23a0b9765ede4a8a9775dbaa46896bd5b196ebd
Hampton Catlin <hcatlin@gmail.com>
2020-12-09 13:19:16 +0000

    add in the audio analyser

--- app/javascript/helpers/broadcast/mixers/audio_mixer.ts
+++ app/javascript/helpers/broadcast/mixers/audio_mixer.ts
@@ -9,6 +9,15 @@ export default class AudioMixer implements Mixer {
   _audioContext: AudioContext;
   audioSources: Map<CaptureSource, AudioNode> = new Map();
   _destinationNode: MediaStreamAudioDestinationNode;
+  private analyser: AnalyserNode;
+  private canvas: HTMLCanvasElement;
+  private canvasContext: CanvasRenderingContext2D;
+
+  constructor() {
+    this.canvas = document.querySelector("#audioCanvas") as HTMLCanvasElement;
+    this.canvasContext = this.canvas.getContext("2d");
+    this.renderCanvas();
+  }
 
   public addCaptureSource(audioSource: CaptureSource): void {
     const sourceNode = this.audioContext.createMediaStreamSource(
@@ -16,6 +25,7 @@ export default class AudioMixer implements Mixer {
     );
     console.log("Added audio source", audioSource);
     sourceNode.connect(this.destinationNode);
+    sourceNode.connect(this.analyser);
     this.audioSources.set(audioSource, sourceNode);
   }
 
@@ -29,9 +39,29 @@ export default class AudioMixer implements Mixer {
     return this.destinationNode.stream.getAudioTracks()[0];
   }
 
+  renderCanvas(): void {
+    if (this.analyser) {
+      const array = new Uint8Array(this.analyser.frequencyBinCount);
+
+      this.analyser.getByteFrequencyData(array);
+      const arrAvg = array.reduce((a, b) => a + b, 0) / array.length;
+      // volumeBars.mono.style.height = arrAvg * 2 + "px";
+      // volumeBars.mono.innerHTML = Math.floor(arrAvg);
+      this.canvasContext.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.canvasContext.fillStyle = "green";
+      this.canvasContext.fillRect(0, 0, arrAvg, this.canvas.height);
+    }
+    requestAnimationFrame(() => this.renderCanvas());
+  }
+
   // Lazy-initialize this context until we are ready for it!
   get audioContext(): AudioContext {
-    return this._audioContext || (this._audioContext = new AudioContext());
+    if (this._audioContext) {
+      return this._audioContext;
+    }
+    this._audioContext = new AudioContext();
+    this.setupAnalyser();
+    return this._audioContext;
   }
 
   // Lazy-initialize the destination node until we are ready for it!
@@ -40,5 +70,11 @@ export default class AudioMixer implements Mixer {
       this._destinationNode ||
       (this._destinationNode = this.audioContext.createMediaStreamDestination())
     );
+  }
+
+  private setupAnalyser() {
+    this.analyser = this._audioContext.createAnalyser();
+    this.analyser.smoothingTimeConstant = 0.85;
+    this.analyser.fftSize = 64;
   }
 }
