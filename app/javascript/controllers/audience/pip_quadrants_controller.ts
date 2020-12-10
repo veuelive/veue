c84b6f00cea9a8e002aa782992499c4d1f5a458a
Jason Fleetwood-Boldt <jason.fb@datatravels.com>
2020-12-10 13:12:53 -0500

    removing console log

--- app/javascript/controllers/audience/pip_quadrants_controller.ts
+++ app/javascript/controllers/audience/pip_quadrants_controller.ts
@@ -2,20 +2,13 @@ import { Controller } from "stimulus";
 
 export default class PipQuadrantsController extends Controller {
   connect(): void {
-    // ondragenter is attached by audience_view_controller
-    // this.element.ondragenter = this.onDragEnter.bind(this);
-
+    // ondragenter and onDrop is attached by audience_view_controller
     this.element.ondragleave = this.onDragLeave.bind(this);
     this.element.ondragover = this.onDragOver.bind(this);
-    // this.element.onDrop = this.onDrop.bind(this);
-
     super.connect();
   }
 
-  onDragLeave(event): void {
-    console.log("onDragLeave ", this);
-    console.log("onDragLeave ", event.currentTarget);
-  }
+  onDragLeave(event): void {}
 
   onDragOver(event): void {
     event.preventDefault();
