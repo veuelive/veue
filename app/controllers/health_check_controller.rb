93e7c2d86bf8b5359d6aa6a72cf1917bb7967430
Jason Fleetwood-Boldt <jason.fb@datatravels.com>
2020-12-11 17:42:23 -0500

    rubocop fixes

--- app/controllers/health_check_controller.rb
+++ app/controllers/health_check_controller.rb
@@ -1,14 +1,18 @@
+# frozen_string_literal: true
+
 class HealthCheckController < ApplicationController
   def index
     db_connections_query = ActiveRecord::Base.connection.execute("SELECT count(*) FROM pg_stat_activity WHERE state='active'; ")
     db_connections = db_connections_query.count
     sidekiq_workers = Sidekiq::Workers.new.count
 
-    live_videos = Video.where(state: 'live').count
+    live_videos = Video.where(state: "live").count
 
-    render json: {status: "OK",
-                  db_connections: db_connections,
-                  live_videos: live_videos,
-                  sidekiq_workers: sidekiq_workers}
+    render(json: {
+             status: "OK",
+             db_connections: db_connections,
+             live_videos: live_videos,
+             sidekiq_workers: sidekiq_workers,
+           })
   end
 end
