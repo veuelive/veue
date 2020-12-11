bcb46ca5836ed5b7d2dab601713db57bcaee891f
Jason Fleetwood-Boldt <jason.fb@datatravels.com>
2020-12-11 17:36:45 -0500

    [VEUE-278] adds basic health check at /health endpoint

---
+++ app/controllers/health_check_controller.rb
@@ -0,0 +1,14 @@
+class HealthCheckController < ApplicationController
+  def index
+    db_connections_query = ActiveRecord::Base.connection.execute("SELECT count(*) FROM pg_stat_activity WHERE state='active'; ")
+    db_connections = db_connections_query.count
+    sidekiq_workers = Sidekiq::Workers.new.count
+
+    live_videos = Video.where(state: 'live').count
+
+    render json: {status: "OK",
+                  db_connections: db_connections,
+                  live_videos: live_videos,
+                  sidekiq_workers: sidekiq_workers}
+  end
+end
