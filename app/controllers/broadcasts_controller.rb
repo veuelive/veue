91ae16046224f353a862c1259a8396facda627de
Hampton Catlin <hcatlin@gmail.com>
2020-12-11 14:01:54 +0000

    [VEUE-301] Rubocop Fixes
    In the original version of this, the start method was violating complexity limits, so I’ve broken out this screenshot logic.

--- app/controllers/broadcasts_controller.rb
+++ app/controllers/broadcasts_controller.rb
@@ -13,9 +13,8 @@ class BroadcastsController < ApplicationController
   end
 
   def start
-    current_broadcast_video.primary_shot = params[:primary_shot] if params[:primary_shot]
-    current_broadcast_video.secondary_shot = params[:secondary_shot] if params[:secondary_shot]
-    current_broadcast_video.save!
+    current_broadcast_video.add_screenshots!(params[:primary_shot], params[:secondary_shot])
+
     BrowserNavigation.create_first_navigation!(
       current_broadcast_video,
       params[:url],
