c285bbe33dc5ca6beafaf2e2c5d326a1a1d2f683
Jason Fleetwood-Boldt <jason.fb@datatravels.com>
2020-12-14 12:30:23 -0500

    private-video--vieo-area gets position: relative;  abstract into new parital at app/views/videos/partials/_pip_area.html.haml;

--- app/javascript/controllers/audience/pip_quadrants_controller.ts
+++ app/javascript/controllers/audience/pip_quadrants_controller.ts
@@ -3,18 +3,12 @@ import { Controller } from "stimulus";
 export default class PipQuadrantsController extends Controller {
   connect(): void {
     // ondragenter and onDrop is attached by audience_view_controller
-    console.log("PipQuadrantsController connect.....");
     this.element.ondragleave = this.onDragLeave.bind(this);
     this.element.ondragover = this.onDragOver.bind(this);
-    // this.element.ondragenter = this.onDragEnter.bind(this);
     super.connect();
   }
 
   onDragLeave(event): void {}
-
-  // onDragEnter(event): void {
-  //   console.log("ondragenter.......");
-  // }
 
   onDragOver(event): void {
     event.preventDefault();
