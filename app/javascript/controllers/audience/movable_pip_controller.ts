3b012210a4429e68d64c1d8e45471e9a3f69a820
Hampton Catlin <hcatlin@gmail.com>
2020-12-14 14:32:56 +0000

    [VEUE-239] Hampton’s Take

--- app/javascript/controllers/audience/movable_pip_controller.ts
+++ app/javascript/controllers/audience/movable_pip_controller.ts
@@ -1,4 +1,11 @@
 import { Controller } from "stimulus";
+import { polyfill } from "mobile-drag-drop";
+import { scrollBehaviourDragImageTranslateOverride } from "mobile-drag-drop/scroll-behaviour";
+
+polyfill({
+  // use this to make use of the scroll behaviour
+  dragImageTranslateOverride: scrollBehaviourDragImageTranslateOverride,
+});
 
 export default class MovablePipController extends Controller {
   static targets = [
@@ -8,17 +15,21 @@ export default class MovablePipController extends Controller {
     "bottomLeft",
     "bottomRight",
   ];
+  private pipComponentTarget!: HTMLCanvasElement;
+  private topLeftTarget!: HTMLElement;
+  private topRightTarget!: HTMLElement;
+  private bottomLeftTarget!: HTMLElement;
+  private bottomRightTarget!: HTMLElement;
+
   connect(): void {
-    console.log("MovablePipController... connect"); // does not appear to connect
+    this.movePipTo("bottom-right");
     this.topLeftTarget.ondragenter = () => this.movePipTo("top-left");
     this.topRightTarget.ondragenter = () => this.movePipTo("top-right");
     this.bottomLeftTarget.ondragenter = () => this.movePipTo("bottom-left");
     this.bottomRightTarget.ondragenter = () => this.movePipTo("bottom-right");
-
-    super.connect();
   }
 
   movePipTo(corner: string): void {
-    this.pipComponentTarget.setAttribute("data-corner-position", corner);
+    this.pipComponentTarget.dataset.corner = corner;
   }
 }
