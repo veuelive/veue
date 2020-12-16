6761576272b5753788d076bc3ec99d9b9ef463c0
Sirbuland <sirbuland.atlas@gmail.com>
2020-12-16 16:53:35 +0500

    [VEUE-293] change naming convention to snake case

--- app/javascript/controllers/broadcast/startup_controller.ts
+++ app/javascript/controllers/broadcast/startup_controller.ts
@@ -21,7 +21,7 @@ export default class extends BaseController {
     this.subscribeToAuthChange();
     ipcRenderer.send("check_access");
 
-    ipcRenderer.on("access-status", (_, accessStatus) => {
+    ipcRenderer.on("access_status", (_, accessStatus) => {
       if (!accessStatus.isCameraAccessible) {
         this.videoAccessTarget.textContent = "✗";
         this.userInstructionsModalTarget.setAttribute(
