93210678d239e14209895b5ff3dd46537222da0b
ParamagicDev <konnor5456@gmail.com>
2020-12-21 09:17:42 -0500

    remove decorate metadata, not recommended by lit-element

---
+++ app/javascript/components/veue-select/index.ts
@@ -0,0 +1,5 @@
+import VeueSelect from "./src/VeueSelect";
+
+window.customElements.define("veue-select", VeueSelect);
+
+export { default } from "./src/VeueSelect";
---
+++ web-test-runner.config.mjs
@@ -0,0 +1,4 @@
+export default {
+  files: 'spec/javascript/components/**/*.spec.js',
+  nodeResolve: true
+};
