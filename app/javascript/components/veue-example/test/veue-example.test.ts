cff1755638904a3348ad9bf284cedd23c2161720
ParamagicDev <konnor5456@gmail.com>
2020-12-21 17:22:59 -0500

    fix tests

--- app/javascript/components/veue-example/test/veue-example.test.ts
+++ app/javascript/components/veue-example/test/veue-example.test.ts
@@ -5,14 +5,18 @@ import "../index";
 
 describe("VeueExample", () => {
   it('has a default title "Hey there" and counter 1', async () => {
-    const el: VeueExample = await fixture(html` <veue-select></veue-select> `);
+    const el: VeueExample = await fixture(
+      html` <veue-example></veue-example> `
+    );
 
     expect(el.title).to.equal("Hey there");
     expect(el.counter).to.equal(0);
   });
 
   it("increases the counter on button click", async () => {
-    const el: VeueExample = await fixture(html` <veue-select></veue-select> `);
+    const el: VeueExample = await fixture(
+      html` <veue-example></veue-example> `
+    );
     el.shadowRoot!.querySelector("button")!.click();
 
     expect(el.counter).to.equal(1);
@@ -20,14 +24,16 @@ describe("VeueExample", () => {
 
   it("can override the title via attribute", async () => {
     const el: VeueExample = await fixture(html`
-      <veue-select title="attribute title"></veue-select>
+      <veue-example title="attribute title"></veue-example>
     `);
 
     expect(el.title).to.equal("attribute title");
   });
 
   it("passes the a11y audit", async () => {
-    const el: VeueExample = await fixture(html` <veue-select></veue-select> `);
+    const el: VeueExample = await fixture(
+      html` <veue-example></veue-example> `
+    );
 
     expect(el).shadowDom.to.be.accessible();
   });
