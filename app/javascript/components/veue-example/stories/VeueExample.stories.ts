e82184cd43e8f97ab9c353d6fc47915c1568f6f9
ParamagicDev <konnor5456@gmail.com>
2020-12-22 15:14:56 -0500

    fix for eslint

--- app/javascript/components/veue-example/stories/VeueExample.stories.ts
+++ app/javascript/components/veue-example/stories/VeueExample.stories.ts
@@ -1,4 +1,4 @@
-import { html } from "lit-element";
+import { html, TemplateResult } from "lit-element";
 import "../index";
 
 export default {
@@ -10,24 +10,30 @@ export default {
   },
 };
 
-const Template = ({ title, text, counter = 0 }) => {
+interface ITemplate {
+  title?: string;
+  text?: string;
+  counter?: number;
+}
+const Template = ({ title, text, counter = 0 }: ITemplate): TemplateResult => {
   return html`
     <veue-example .title=${title} .counter=${counter}>${text}</veue-example>
   `;
 };
 
-export const MyElement = (args) => Template(args);
+export const MyElement = (args: ITemplate): TemplateResult => Template(args);
 MyElement.args = {
   title: "My title",
 };
 
-export const NegativeCounter = (args) => Template(args);
+export const NegativeCounter = (args: ITemplate): TemplateResult =>
+  Template(args);
 NegativeCounter.args = {
   title: "Foo",
   counter: -5,
 };
 
-export const SlottedText = (args) => Template(args);
+export const SlottedText = (args: ITemplate): TemplateResult => Template(args);
 SlottedText.args = {
   title: "My title",
   text: "Some slotted text",
