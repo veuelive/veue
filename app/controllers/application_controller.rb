e75f6b6419b341ef586b0fbdcf932e26accb3287
Hampton Catlin <hcatlin@gmail.com>
2020-12-26 11:04:32 +0000

    Had some Rubocop issues that weren’t address

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   include HttpAuthConcern
   include AuthenticationConcern
   include IpcMockConcern unless Rails.env.production?
+
   private
 
   def xhr_request?
