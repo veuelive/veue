3fe21d9b0b5111f7a2836984bc52fe47343a23ee
Sirbuland <sirbuland.atlas@gmail.com>
2020-12-28 19:36:37 +0500

    [VEUE-305] adds cert secret and callbacks for autoUpdater

--- broadcaster/util/autoUpdater.js
+++ broadcaster/util/autoUpdater.js
@@ -1,7 +1,30 @@
 import { autoUpdater } from "electron-updater";
 import logger from "../src/logger";
 
-export default function appUpdater() {
+const { dialog } = require("electron");
+export function appUpdater() {
   logger.info("Updating Veue Broadcaster");
   autoUpdater.checkForUpdatesAndNotify();
+
+  autoUpdater.on("update-downloaded", (ev, info) => {
+    // Wait 5 seconds, then quit and install
+    setTimeout(function () {
+      autoUpdater.quitAndInstall();
+    }, 5000);
+  });
+
+  autoUpdater.on("update-available", (info) => {
+    dialog.showMessageBoxSync({
+      title: "update available",
+      message: "New Version of application is available.",
+    });
+  });
+
+  autoUpdater.on("error", (err) => {
+    logger.log({
+      level: "error",
+      message: "Error occurred while updating application.",
+      data: err,
+    });
+  });
 }
