e69d3a17308bd1d5e44e3a948519fbcb2164b95c
Hampton Catlin <hcatlin@gmail.com>
2021-01-04 16:42:31 +0000

    [VEUE-293] adds check for media access
    # Conflicts:
    #	broadcaster/src/main.js

---
+++ broadcaster/util/mediaAccess.js
@@ -0,0 +1,22 @@
+const { systemPreferences } = require("electron");
+
+export async function askForMediaAccess() {
+  const isCameraAccessible =
+    systemPreferences.getMediaAccessStatus("camera") === "granted";
+  const isMicrophoneAccessible =
+    systemPreferences.getMediaAccessStatus("camera") === "granted";
+
+  if (!isCameraAccessible) {
+    isCameraAccessible = await systemPreferences.askForMediaAccess("camera");
+  }
+  if (!isMicrophoneAccessible) {
+    isMicrophoneAccessible = await systemPreferences.askForMediaAccess(
+      "microphone"
+    );
+  }
+
+  return {
+    isCameraAccessible,
+    isMicrophoneAccessible,
+  };
+}
