e4a7dd42a52a7edfade4b3342e36e658cff81cd0
Seraphyne <renatafd717@gmail.com>
2021-01-19 10:05:08 -0600

    VEUE-287 - changes on PR

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -68,7 +68,8 @@ module VideosHelper
   end
 
   def seconds_to_time(seconds)
-    return unless seconds  
+    return unless seconds
+
     [seconds / 3600, seconds / 60 % 60, seconds % 60].map { |t| t.to_s.rjust(2, "0") }.join(":")
   end
 end
