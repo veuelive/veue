dc7ae0c8275b6f258aaf8dd2ee7a44a34a21397c
Hampton Catlin <hcatlin@gmail.com>
2021-01-26 14:57:27 +0000

    Spelling Tweak.

--- app/controllers/internal/cron_controller.rb
+++ app/controllers/internal/cron_controller.rb
@@ -7,7 +7,7 @@ module Internal
       logger.info "Cancelling #{stale_videos.count} videos"
       stale_videos.each(&:cancel!)
 
-      render(json: {ok: true, canceled_videos: stale_videos.count})
+      render(json: {ok: true, cancelled_videos: stale_videos.count})
     end
   end
 end
