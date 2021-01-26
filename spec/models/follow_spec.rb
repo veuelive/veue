7bed6c6d88dd5ca69d97e2b83cf1d962ffb7685d
Seraphyne <renatafd717@gmail.com>
2021-01-26 09:35:42 -0600

    VEUE-429 - PR changes - title of a spec

--- spec/models/follow_spec.rb
+++ spec/models/follow_spec.rb
@@ -35,7 +35,7 @@ RSpec.describe Follow, type: :model do
     expect(channel_two.followers).to include(user)
   end
 
-  it "User cannot follow themselves" do
+  it "should prevent user from following themselves" do
     follow = build(:follow, user: channel_one.user, channel: channel_one)
     expect(follow).to be_invalid
     expect(follow.errors.full_messages.join("\n")).to include("You can't follow yourself")
