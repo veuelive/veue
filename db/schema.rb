f86b13bf9c04caa6efa825dfa86a8897305659e7
Hampton Catlin <hcatlin@gmail.com>
2021-01-27 13:41:33 +0000

    use actual references

--- db/schema.rb
+++ db/schema.rb
@@ -77,6 +77,9 @@ ActiveRecord::Schema.define(version: 2021_01_25_173557) do
     t.uuid "video_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_moderation_items_on_user_id"
+    t.index ["video_event_id"], name: "index_moderation_items_on_video_event_id"
+    t.index ["video_id"], name: "index_moderation_items_on_video_id"
   end
 
   create_table "mux_webhooks", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
