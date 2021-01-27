4c0fe14fe7d1bb0dc17ead710c55f45105949d7a
ParamagicDev <konnor5456@gmail.com>
2021-01-27 11:44:11 -0500

    make the cops happy

--- app/decorators/video_decorator.rb
+++ app/decorators/video_decorator.rb
@@ -44,9 +44,7 @@ class VideoDecorator < Draper::Decorator
     time = helpers.time_ago_in_words(started_at)
 
     # If its 'yesterday' we dont need to add the suffix
-    if time == I18n.t('datetime.distance_in_words.x_days.one')
-      return time
-    end
+    return time if time == I18n.t("datetime.distance_in_words.x_days.one")
 
     "#{time} #{suffix}"
   end
