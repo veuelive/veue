706d9093ccba36e00896e93b18f03afbea08a2ef
ParamagicDev <konnor5456@gmail.com>
2021-02-01 16:18:35 -0500

    [VEUE-487]: Dont create user joined events for VODs

---
+++ spec/requests/user_joined_events_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Channels::VideosController do
+  describe "user joined events" do
+    before(:each) do
+      @viewer = create(:user)
+      login_as @viewer
+    end
+
+    describe "for live videos" do
+      it "should create a new user joined event" do
+        video = create(:live_video)
+        expect(video.user_joined_events.size).to eq(0)
+        get path_for_video(video)
+        expect(video.user_joined_events.reload.size).to eq(1)
+      end
+    end
+
+    describe "for vods" do
+      it "should not create a new user joined event" do
+        video = create(:vod_video)
+        expect(video.user_joined_events.size).to eq(0)
+        get path_for_video(video)
+        expect(video.user_joined_events.reload.size).to eq(0)
+      end
+    end
+  end
+end
