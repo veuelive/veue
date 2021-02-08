83f2578ae9bf1a798987d7a3c5b6e2bdcb46df57
ParamagicDev <konnor5456@gmail.com>
2021-02-08 13:18:36 -0500

    remove ip_Address and email uniqueness

--- db/migrate/20210204212826_add_email_encryption.rb
+++ db/migrate/20210204212826_add_email_encryption.rb
@@ -5,8 +5,7 @@ class AddEmailEncryption < ActiveRecord::Migration[6.0]
     change_table(:users, bulk: true) do |t|
       t.column(:email_ciphertext, :text)
       t.column(:email_bidx, :string)
-      t.index(:email, unique: true, where: "email IS NOT NULL")
-      t.index(:email_bidx, unique: true, where: "email_bidx IS NOT NULL")
+      t.index(:email_bidx)
     end
   end
 end
