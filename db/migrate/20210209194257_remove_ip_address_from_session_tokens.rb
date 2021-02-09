d5af6de32ecee8bdb53dac95df9b4eaa75bd6e8b
ParamagicDev <konnor5456@gmail.com>
2021-02-09 15:24:47 -0500

    add a typing to remove_column for rollback purposes

--- db/migrate/20210209194257_remove_ip_address_from_session_tokens.rb
+++ db/migrate/20210209194257_remove_ip_address_from_session_tokens.rb
@@ -1,5 +1,5 @@
 class RemoveIpAddressFromSessionTokens < ActiveRecord::Migration[6.0]
   def change
-    remove_column :session_tokens, :ip_address
+    remove_column :session_tokens, :ip_address, :inet
   end
 end
