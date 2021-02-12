7655aabb7ba640c43736a74ffdb7c6d3eb0eed2e
ParamagicDev <konnor5456@gmail.com>
2021-02-12 13:25:00 -0500

    fix migrations

--- db/migrate/20210113153135_add_by_streamer_flag_in_payload_of_chat_message_event.rb
+++ db/migrate/20210113153135_add_by_streamer_flag_in_payload_of_chat_message_event.rb
@@ -1,5 +1,8 @@
 class AddByStreamerFlagInPayloadOfChatMessageEvent < ActiveRecord::Migration[6.0]
   def change
+    add_column :video_events, :published, :boolean, default: true
+    add_index :video_events, [:video_id, :published, :timecode_ms, :type], name: :video_event_big_index
+
     ChatMessage.all.each do |message|
       message.set_payload
       message.save
