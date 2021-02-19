ac0a239a95ac6d9153b529208349c3eef363e9e9
ParamagicDev <konnor5456@gmail.com>
2021-02-19 13:28:14 -0500

    remove perspective api check, its not needed

--- app/controllers/concerns/moderate_concern.rb
+++ app/controllers/concerns/moderate_concern.rb
@@ -2,8 +2,6 @@ module ModerateConcern
   extend ActiveSupport::Concern
 
   def create_moderation_item(text:, **options)
-    return unless PerspectiveApi.enabled
-
     moderation_item = ModerationItem.create!(text: text, **options)
     moderation_item.fetch_scores!
     moderation_item
