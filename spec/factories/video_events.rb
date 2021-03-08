4f672f5997b01f8875e5057eadc86447d4d01bba
Hampton Catlin <hcatlin@gmail.com>
2021-03-08 11:04:13 -0500

    Revert "add playback specs"
    This reverts commit da67d5627cf847fbf7fff1fff9776fbe2760fc65.

--- spec/factories/video_events.rb
+++ spec/factories/video_events.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
-def random_boolean
-  [true, false].sample
-end
-
 FactoryBot.define do
   factory :video_layout_event do
     # This is the layout of the video in public/__test/vod
@@ -38,18 +34,5 @@ FactoryBot.define do
     video { create(:video) }
     user { create(:user) }
     input { {message: Faker::Marketing.buzzwords} }
-  end
-
-  factory :browser_navigation do
-    video { create(:video) }
-    user { create(:user) }
-    input {
-      {
-        url: Faker::Internet.url,
-        canGoBack: random_boolean,
-        canGoForward: random_boolean,
-        isLoading: random_boolean,
-      }
-    }
   end
 end
