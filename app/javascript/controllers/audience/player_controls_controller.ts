274398ae082da066b6350e4684058de59f56c82d
Hampton Catlin <hcatlin@gmail.com>
2021-03-08 11:04:13 -0500

    Revert "minor cleanup"
    This reverts commit ca337f26e2a4796d5cb43da63df687275f4558d9.

--- app/javascript/controllers/audience/player_controls_controller.ts
+++ app/javascript/controllers/audience/player_controls_controller.ts
@@ -4,6 +4,7 @@ import {
   timecodeChangeEvent,
   playbackTimeChangeEvent,
 } from "util/time";
+import { VideoEventProcessor } from "helpers/event/event_processor";
 
 export default class extends BaseController {
   static targets = [
@@ -130,6 +131,7 @@ export default class extends BaseController {
   }
 
   dispatchPlaybackChange(timecodeMs: number): void {
+    console.log("PLAYCHANGE: ", timecodeMs);
     this.element.dispatchEvent(
       new CustomEvent(playbackTimeChangeEvent, { detail: { timecodeMs } })
     );
