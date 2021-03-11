d01a5362168f0b749955fc9d6ee4cf357eaecddb
ParamagicDev <konnor5456@gmail.com>
2021-03-11 18:27:25 -0500

    [VEUE-631]: Transition videos from scheduled to starting
    add test for scheduled videos to transition to starting
    
    fix test

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -25,6 +25,14 @@ RSpec.describe Video, type: :model do
       expect(video.scheduled_at).to be_nil
     end
 
+    it "should allow you to start a scheduled video" do
+      one_week_from_now = 1.week.from_now
+      video.update!(scheduled_at: one_week_from_now)
+      video.start!
+      expect(video.scheduled?).to be(false)
+      expect(video.starting?).to be(true)
+    end
+
     it "should not allow you to schedule in the past" do
       yesterday = 1.day.ago
       expect { video.update!(scheduled_at: yesterday) }.to raise_error(ActiveRecord::RecordInvalid)
