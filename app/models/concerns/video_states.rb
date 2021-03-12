0b369c563f00e627ee9ce7f46004bc10fb2ded9f
Konnor Rogers <konnor5456@gmail.com>
2021-03-11 19:37:25 -0500

    Update video_states.rb

--- app/models/concerns/video_states.rb
+++ app/models/concerns/video_states.rb
@@ -49,7 +49,7 @@ module VideoStates
           after_go_live
         end
 
-        transitions from: :pending, to: :live
+        transitions from: %i[pending starting], to: :live
       end
 
       event :end do
