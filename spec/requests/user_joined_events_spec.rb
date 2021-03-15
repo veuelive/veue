19bcc273e592dd8eb0e193ecfe4232960c937dda
Hampton Catlin <hcatlin@gmail.com>
2021-03-15 13:47:49 -0400

    remove request specs and replace with models

--- spec/requests/user_joined_events_spec.rb
+++ spec/requests/user_joined_events_spec.rb
@@ -20,18 +20,6 @@ describe Channels::VideosController do
         post viewed_channel_video_url(video.channel, video, minute: 1)
         expect(user_joined_events.reload.size).to eq(1)
       end
-
-      it "should not create a user_joined_event if less than 5 seconds have passed since the previous" do
-        expect(Rails.cache).to receive(:fetch).and_return(Integer(3.seconds.ago))
-        post viewed_channel_video_url(video.channel, video, minute: 1)
-        expect(video.user_joined_events.published.reload.size).to eq(0)
-      end
-
-      it "should create a 2nd_user_joined event if greater than 5 seconds have passed" do
-        expect(Rails.cache).to receive(:fetch).and_return(Integer(6.seconds.ago))
-        post viewed_channel_video_url(video.channel, video, minute: 1)
-        expect(video.user_joined_events.published.reload.size).to eq(1)
-      end
     end
 
     describe "for vods" do
