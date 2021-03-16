23874e51011b80fcc9ac205f37d0f01762ef6fe1
Hampton Catlin <hcatlin@gmail.com>
2021-03-16 16:41:00 -0400

    [VEUE-640] High Quality Webcam
    This PR ensures that we ask the OS for the highest possible video quality!

--- app/javascript/helpers/broadcast/capture_sources/webcam.ts
+++ app/javascript/helpers/broadcast/capture_sources/webcam.ts
@@ -11,14 +11,47 @@ export class WebcamCaptureSource extends VideoCaptureSource {
     return document.querySelector("#webcam_preview");
   }
 
+  async findMediaDeviceInfo(): Promise<MediaDeviceInfo> {
+    const devices = await navigator.mediaDevices.enumerateDevices()
+    let deviceId = this.deviceId || "default"
+
+    console.log(devices)
+
+    const videoDevices = devices.filter((value) => value.kind === "videoinput")
+
+    const device = videoDevices.find((mediaDevice) => mediaDevice.deviceId === deviceId)
+
+    if(device) {
+      return device
+    } else if(videoDevices.length > 0) {
+      return videoDevices[0]
+    } else {
+      return Promise.reject("No video media device found")
+    }
+  }
+
+  async getCapabilities(): Promise<MediaTrackCapabilities> {
+    const mediaDeviceInfo = await this.findMediaDeviceInfo();
+
+    // Typescript is convinced that this API doesn't exist, but it DOES in electron
+    // so here we are casting this to a MediaStreamTrack, even though it isn't one.
+    return (mediaDeviceInfo as unknown as MediaStreamTrack).getCapabilities()
+  }
+
   async start(): Promise<void> {
+
+    const capabilities = await this.getCapabilities()
     this.mediaStream = await navigator.mediaDevices.getUserMedia({
       video: {
         deviceId: this.deviceId || "default",
+        height: capabilities.height.max,
+        width: capabilities.width.max
       },
       audio: false,
     });
+    console.log(this.mediaStream)
     await this.processMediaStream(this.mediaStream);
+    console.log("Video element", this.element)
     this.layout = {
       width: this.element.videoWidth,
       height: this.element.videoHeight,
