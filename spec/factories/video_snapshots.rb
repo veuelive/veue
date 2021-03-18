39d6a8fef83fbc1880a444944dde9cb407dddaed
ParamagicDev <konnor5456@gmail.com>
2021-03-18 12:38:29 -0400

    fix test factory

--- spec/factories/video_snapshots.rb
+++ spec/factories/video_snapshots.rb
@@ -3,8 +3,7 @@
 FactoryBot.define do
   factory :video_snapshot do
     video { create(:video) }
-    # image { Rack::Test::UploadedFile.new("spec/factories/test.png", "image/png") }
-    image { Rack::Test::UploadedFile.new("spec/fixtures/profile.jpg", "image/jpg") }
+    image { Rack::Test::UploadedFile.new("spec/factories/test.png", "image/png") }
     timecode { Faker::Number.within(range: 1..100_000) }
     viewer_count { Faker::Number.within(range: 1..100_000) }
     device_id { SecureRandom.uuid }
