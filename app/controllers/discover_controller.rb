6ba336cdb57d3fe9427ef9ce58af7005850b3b4b
Hampton Lintorn Catlin <hcatlin@veuelive.com>
2021-03-22 18:40:57 -0400

    MUST GIVE TO THE LINTER GODS

--- app/controllers/discover_controller.rb
+++ app/controllers/discover_controller.rb
@@ -3,9 +3,12 @@
 class DiscoverController < ApplicationController
   # GET /discover or /
   def index
-    @live_videos = Video.live.visibility_public.most_recent.limit(3).decorate
-    @recent_videos = Video.finished.visibility_public.most_recent.decorate
+    # BE CAREFUL only to use this `public_videos` query builder!
+    public_videos = Video.visibility_public.most_recent
+
+    @live_videos = public_videos.live.limit(3).decorate
+    @recent_videos = public_videos.finished.decorate
     @popular_channels = Channel.most_popular.limit(8).decorate
-    @upcoming_videos = Video.scheduled.visibility_public.order("scheduled_at asc").limit(12).decorate
+    @upcoming_videos = public_videos.scheduled.order("scheduled_at asc").limit(12).decorate
   end
 end
