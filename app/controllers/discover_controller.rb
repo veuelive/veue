2f09e8af09717653b308e4903dac7ceb15c66013
Hampton Lintorn Catlin <hcatlin@veuelive.com>
2021-03-23 16:29:41 -0400

    [VEUE-654] Curations on Homepage
    Now, Seth can go into ButterCMS and create a special type of "Component" in the CMS that will display a new "strip" on the homepage

--- app/controllers/discover_controller.rb
+++ app/controllers/discover_controller.rb
@@ -6,9 +6,41 @@ class DiscoverController < ApplicationController
     # BE CAREFUL only to use this `public_videos` query builder!
     public_videos = Video.visibility_public.most_recent
 
+    load_curations
+
     @live_videos = public_videos.live.limit(3).decorate
     @recent_videos = public_videos.finished.decorate
     @popular_channels = Channel.most_popular.limit(8).decorate
     @upcoming_videos = public_videos.scheduled.order("scheduled_at asc").limit(12).decorate
+  end
+
+  def load_curations
+    cms_data = fetch_curations
+
+    @curations =
+      cms_data.data.fields.curations.map do |curation|
+        map_curation_to_videos(curation)
+      end
+  rescue ButterCMS::NotFound
+    Rails.logger.error("Couldn't load curations")
+  end
+
+  def fetch_curations
+    ButterCMS::Page.get(
+      "*",
+      "homepage-en",
+      {
+        preview: params[:preview],
+      },
+      )
+  end
+
+  def map_curation_to_videos(curation)
+    video_ids = curation.fields.videos.map(&:video_id)
+    videos = Video.where(id: video_ids).visibility_public.all.decorate
+    {
+      name: curation.fields.curation_name,
+      videos: videos,
+    }
   end
 end
