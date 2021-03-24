d54504786fdb5837144dc72833bed4239288d2ff
ParamagicDev <konnor5456@gmail.com>
2021-03-24 15:26:35 -0400

    [VEUE-641]: Add authorization testing

--- spec/models/channel_spec.rb
+++ spec/models/channel_spec.rb
@@ -3,6 +3,11 @@
 require "rails_helper"
 
 RSpec.describe Channel, type: :model do
+  let(:channel) { create(:channel) }
+  let(:user) { channel.user }
+  let(:random_user) { create(:user) }
+  let(:admin_user) { create(:admin) }
+
   describe "creating a new channel" do
     def display_name_to_slug(display_name, slug)
       user = create(:user, display_name: display_name)
@@ -16,6 +21,32 @@ RSpec.describe Channel, type: :model do
       display_name_to_slug("Hampton", "hampton")
       display_name_to_slug("Slüggy Slugers0n!-", "sluggy-slugers0n")
       display_name_to_slug("hampton", "hampton-live")
+    end
+  end
+
+  describe "authorization" do
+    it "Should allow users to edit their own channel and allow admins to edit any channel" do
+      abilities = [Ability.new(user), Ability.new(admin_user)]
+
+      abilities.each do |ability|
+        %i[manage create read update destroy].each do |action|
+          expect(ability.can?(action, channel)).to eq(true)
+        end
+      end
+    end
+
+    it "Should only allow non-users and users who do not have access read permission to a channel" do
+      abilities = [Ability.new(random_user), Ability.new(nil)]
+
+      abilities.each do |ability|
+        # A random / non-user should be able to only read the video.
+        expect(ability.can?(:read, channel)).to eq(true)
+
+        # A random user cannot
+        %i[manage create update destroy].each do |action|
+          expect(ability.cannot?(action, channel)).to eq(true)
+        end
+      end
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,9 +5,38 @@ require "rails_helper"
 RSpec.describe User, type: :model do
   # Use streamer since streamers are users with more permissions
   # An issue was found specific to mux_webhooks not being referenced in our migrations
-  let!(:user) { create(:streamer) }
-
   it "should allow us to delete users" do
+    user = create(:streamer)
     expect { user.destroy! }.to change { User.count }.from(1).to(0)
+  end
+
+  describe "authorization" do
+    let(:user) { create(:user) }
+    let(:random_user) { create(:user) }
+    let(:admin_user) { create(:admin) }
+
+    it "Should allow users to edit their own profile and allow admins to edit any user" do
+      abilities = [Ability.new(user), Ability.new(admin_user)]
+
+      abilities.each do |ability|
+        %i[manage create read update destroy].each do |action|
+          expect(ability.can?(action, user)).to eq(true)
+        end
+      end
+    end
+
+    it "Should only allow non-users and users who do not have access read permission to a profile" do
+      abilities = [Ability.new(random_user), Ability.new(nil)]
+
+      abilities.each do |ability|
+        # A random / non-user should be able to only read the video.
+        expect(ability.can?(:read, user)).to eq(true)
+
+        # A random user cannot
+        %i[manage create update destroy].each do |action|
+          expect(ability.cannot?(action, user)).to eq(true)
+        end
+      end
+    end
   end
 end
