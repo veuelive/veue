b296da47f22aaf70193119f443c23c7edc36cc68
Hampton Lintorn Catlin <hcatlin@veuelive.com>
2021-03-29 19:07:07 -0400

    force unique values for chat tests

--- spec/support/audience_spec_helpers.rb
+++ spec/support/audience_spec_helpers.rb
@@ -29,7 +29,7 @@ module AudienceSpecHelpers
     page.has_css?(audience_audio_state_css("unmuted"))
   end
 
-  def assert_video_is_playing(seconds=2)
+  def assert_video_is_playing(seconds=1)
     expect(page).to have_css("*[data-audience-view-state='playing']")
     expect(page).to have_content(:all, "00:00:#{seconds.to_s.rjust(2, '0')}", wait: 10)
   end
