5b4a86af7ecf09640a8c7bc789c1655c460b5fe7
Hampton Lintorn Catlin <hcatlin@veuelive.com>
2021-03-29 16:46:30 -0400

    wait a little longer

--- spec/support/audience_spec_helpers.rb
+++ spec/support/audience_spec_helpers.rb
@@ -29,9 +29,9 @@ module AudienceSpecHelpers
     page.has_css?(audience_audio_state_css("unmuted"))
   end
 
-  def assert_video_is_playing(seconds=1)
+  def assert_video_is_playing(seconds=2)
     expect(page).to have_css("*[data-audience-view-state='playing']")
-    expect(page).to have_content(:all, "00:00:#{seconds.to_s.rjust(2, '0')}", wait: 5)
+    expect(page).to have_content(:all, "00:00:#{seconds.to_s.rjust(2, '0')}", wait: 10)
   end
 
   def current_timecode
