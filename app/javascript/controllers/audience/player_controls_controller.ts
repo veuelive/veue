e42bcb8c7f9e35ca87ab914ffa41c497f035c8a8
ParamagicDev <konnor5456@gmail.com>
2021-03-30 17:07:47 -0400

    [VEUE-603]: Add timecode and video previews to VODs
    fix tests now that snapshots are viewable
    
    working on screenshots'
    
    working on final pieces of snapshots
    
    video previews!
    
    almost all set!
    
    add spec request tests
    
    Add tests for previews
    
    fix linting errors

--- app/javascript/controllers/audience/player_controls_controller.ts
+++ app/javascript/controllers/audience/player_controls_controller.ts
@@ -1,21 +1,28 @@
 import BaseController from "controllers/base_controller";
 import { displayTime } from "util/time";
 import { VideoSeekEvent } from "helpers/video_helpers";
+import { secureFetch } from "util/fetch";
 
 export default class extends BaseController {
   static targets = [
     "video",
+    "videoPreviewContainer",
+    "videoPreviewImage",
     "audienceView",
     "progressBar",
     "progressBarContainer",
     "progressBarButton",
     "timeDuration",
     "timeDisplay",
+    "timePreview",
   ];
 
   readonly videoTarget!: HTMLVideoElement;
+  readonly videoPreviewImageTarget!: HTMLImageElement;
+  readonly videoPreviewContainerTarget!: HTMLDivElement;
   readonly timeDurationTarget!: HTMLElement;
   readonly timeDisplayTarget!: HTMLElement;
+  readonly timePreviewTarget!: HTMLDivElement;
   readonly audienceViewTarget!: HTMLElement;
   readonly progressBarTarget!: HTMLElement;
   readonly progressBarContainerTarget!: HTMLElement;
@@ -65,22 +72,52 @@ export default class extends BaseController {
     }
   }
 
+  displayPreview(event: PointerEvent): void {
+    event.preventDefault();
+
+    const currentTime = this.getPointerLocationTime(event);
+
+    if (currentTime == null || currentTime < -1) {
+      return;
+    }
+
+    window.requestAnimationFrame(async () => {
+      this.displayTimeAndVideoPreviews(event, currentTime);
+      await this.displaySnapshot(currentTime);
+    });
+  }
+
+  handlePointerEnter(event: PointerEvent): void {
+    event.preventDefault();
+
+    this.displayPreview(event);
+  }
+
+  handlePointerLeave(event: PointerEvent): void {
+    event.preventDefault();
+
+    this.timePreviewTarget.style.display = "none";
+    this.videoPreviewContainerTarget.style.display = "none";
+  }
+
   handlePointerDown(event: PointerEvent): void {
     event.preventDefault();
 
     this.pointerIsDown = true;
-    this.handlePointerLocation(event);
+    this.updateVideoTime(event);
   }
 
   handlePointerMove(event: PointerEvent): void {
     event.preventDefault();
 
-    // The pointer has to be down for us to register a pointermove
+    this.displayPreview(event);
+
+    // The pointer has to be held down for us to update the video time
     if (this.pointerIsDown !== true) {
       return;
     }
 
-    this.handlePointerLocation(event);
+    this.updateVideoTime(event);
   }
 
   handlePointerUp(): void {
@@ -105,23 +142,68 @@ export default class extends BaseController {
       });
   }
 
-  handlePointerLocation(event: PointerEvent): void {
+  updateVideoTime(event: PointerEvent): void {
+    const currentTime = this.getPointerLocationTime(event);
+    this.timeDisplayTarget.innerHTML = displayTime(currentTime);
+    this.videoTarget.currentTime = currentTime;
+    this.handleTimeUpdate();
+  }
+
+  getPointerLocationTime(event: PointerEvent): number {
     const frameRect = this.progressBarContainerTarget.getBoundingClientRect();
 
     // find the offset of the progressbar and the actual X location of the event
     const x = event.clientX - frameRect.left;
     const pos = x / frameRect.width;
+    return pos * this.duration;
+  }
 
-    const currentTime = pos * this.duration;
+  private displayTimeAndVideoPreviews(
+    event: PointerEvent,
+    currentTime: number
+  ): void {
+    const containerOffset = this.progressBarContainerTarget.getBoundingClientRect()
+      .x;
+    const timePreviewOffset =
+      this.timePreviewTarget.getBoundingClientRect().width / 2;
 
-    this.timeDisplayTarget.innerHTML = displayTime(currentTime);
-    this.videoTarget.currentTime = currentTime;
-    this.handleTimeUpdate();
-    const evt = new CustomEvent(VideoSeekEvent, {
-      bubbles: true,
-      detail: { timecodeMs: currentTime },
-    });
-    this.videoTarget.dispatchEvent(evt);
+    const x = event.clientX - containerOffset;
+    const timePreviewX = x - timePreviewOffset;
+    this.timePreviewTarget.style.transform = `translate(${timePreviewX}px, -100%)`;
+    this.timePreviewTarget.innerText = displayTime(currentTime);
+    this.timePreviewTarget.style.display = "block";
+
+    const videoPreviewOffset =
+      this.videoPreviewContainerTarget.getBoundingClientRect().width / 2;
+    const videoPreviewX = x - videoPreviewOffset;
+
+    this.videoPreviewContainerTarget.style.transform = `translate(${videoPreviewX}px, -100%)`;
+    this.videoPreviewContainerTarget.style.display = "block";
+  }
+
+  private async displaySnapshot(timecode: number) {
+    try {
+      const response = await secureFetch(
+        `${document.location.pathname}/snapshots/find?t=${Math.floor(
+          timecode
+        )}`,
+        {
+          headers: { Accept: "application/json" },
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+
+        if (data.url != null) {
+          this.videoPreviewImageTarget.src = data.url;
+          this.videoPreviewImageTarget.style.display = "block";
+          this.videoPreviewImageTarget.dataset.id = data.id;
+        }
+      }
+    } catch (err) {
+      console.warn(err);
+    }
   }
 
   set videoState(state: string) {
