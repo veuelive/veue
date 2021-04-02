0d48617ba3037982a1ebdd7c40bd1b1c3d94ca19
ParamagicDev <konnor5456@gmail.com>
2021-04-02 19:37:27 -0400

    Add tests for previews

--- spec/system/vod/scrubber_previews_spec.rb
+++ spec/system/vod/scrubber_previews_spec.rb
@@ -2,9 +2,9 @@
 
 require "system_helper"
 
-describe "Snapshot previews" do
+describe "Scrubber previews" do
   # Make the video 100 seconds long
-  let(:video) { create(:vod_video, duration: 100) }
+  let(:video) { create(:vod_video) }
 
   before(:each) do
     driven_by :media_browser
@@ -14,13 +14,13 @@ describe "Snapshot previews" do
   describe "With snapshots" do
     # All need to be eagerly created to be able to show on hover
     let!(:snapshot_one) { create(:video_snapshot, video: video, timecode: 0) }
-    let!(:snapshot_two) { create(:video_snapshot, video: video, timecode: 30_000) }
+    let!(:snapshot_two) { create(:video_snapshot, video: video, timecode: 31_000) }
 
     it "Should show a video preview on mouse hover" do
       find(".progress-bar-container").hover
 
       # first paint can be slow in testing, give it a little extra wait time
-      expect(page).to have_css(".progress-bar__time-preview", wait: 5)
+      expect(page).to have_css(".progress-bar__time-preview")
       expect(page).to have_css(".progress-bar__video-preview")
 
       # Make sure the image has the id of snapshot 1
@@ -28,22 +28,27 @@ describe "Snapshot previews" do
     end
 
     it "Should show the next snapshot at ~ 30 seconds" do
+      # This is super finnicky and totally dependent on the video and viewport size,
+      # so take care if the test video changes!
       script = <<~JS
         const container = document.querySelector(".progress-bar-container")
 
         const { width, x, y } = container.getBoundingClientRect()
 
         // Approximate guess of 30 seconds
-        const offset = width / 3
+        const offset = (width / 2) + 100
         container.dispatchEvent(new PointerEvent("pointerenter", {clientX: x + offset, clientY: y, view: window}))
       JS
 
       execute_script(script)
 
-      expect(page).to have_css(".progress-bar__time-preview", wait: 5)
+      expect(page).to have_css(".progress-bar__time-preview")
       expect(page).to have_css(".progress-bar__video-preview")
 
-      # Make sure the image has the id of snapshot 1
+      # We have to re-execute the script to get it to re-hover
+      execute_script(script)
+
+      # Make sure the image has the id of snapshot 2, this can take a little while to happen.
       expect(page).to have_css("[data-id='#{snapshot_two.id}'")
     end
   end
@@ -53,7 +58,7 @@ describe "Snapshot previews" do
       find(".progress-bar-container").hover
 
       # Preview time and image container should still show
-      expect(page).to have_css(".progress-bar__time-preview", wait: 5)
+      expect(page).to have_css(".progress-bar__time-preview")
       expect(page).to have_css(".progress-bar__video-preview")
 
       expect(find(".progress-bar__video-preview__image", visible: false)).not_to(be_visible)
