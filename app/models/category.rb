b6cc3326c1ec759acab75b6dcee38e3706d73a68
Sirbuland <sirbuland.atlas@gmail.com>
2021-04-08 19:33:53 +0500

    [VEUE-265] add has_many categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,7 @@
 class Category < ApplicationRecord
   validates :title, presence: true
 
+  has_many :categories, dependent: :destroy
   belongs_to :category, optional: true
   has_many :video_categories, dependent: :destroy
   has_many :videos, through: :video_categories
