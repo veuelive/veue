823229e74dd3730eb3e2fd2ca8ca6d0cc971949c
ParamagicDev <konnor5456@gmail.com>
2021-04-12 16:42:54 -0400

    [VEUE-703]: Fix time ago in words on discover page

--- spec/system/discover_spec.rb
+++ spec/system/discover_spec.rb
@@ -68,44 +68,4 @@ describe "Discover View" do
     end
   end
 
-  describe "video display times should display as relative" do
-    # These happen so fast, we have to go backwards in time a little to make them display properly in test
-    let(:one_minute_ago) { create(:vod_video, started_at_ms: 30.seconds.ago.utc.to_ms) }
-    let(:two_minutes_ago) { create(:vod_video, started_at_ms: 1.minute.ago.utc.to_ms) }
-    let(:one_hour_ago) { create(:vod_video, started_at_ms: 1.hour.ago.utc.to_ms) }
-    let(:two_hours_ago) { create(:vod_video, started_at_ms: 2.hours.ago.utc.to_ms) }
-    let(:one_day_ago) { create(:vod_video, started_at_ms: 1.day.ago.utc.to_ms) }
-    let(:two_days_ago) { create(:vod_video, started_at_ms: 2.days.ago.utc.to_ms) }
-    let(:one_week_ago) { create(:vod_video, started_at_ms: 1.week.ago.utc.to_ms) }
-    let(:two_weeks_ago) { create(:vod_video, started_at_ms: 2.weeks.ago.utc.to_ms) }
-    let(:one_month_ago) { create(:vod_video, started_at_ms: 1.month.ago.utc.to_ms) }
-    let(:two_months_ago) { create(:vod_video, started_at_ms: 2.months.ago.utc.to_ms) }
-    let(:one_year_ago) { create(:vod_video, started_at_ms: 12.months.ago.utc.to_ms) }
-    let(:two_years_ago) { create(:vod_video, started_at_ms: 2.years.ago.utc.to_ms) }
-
-    it "should show the proper relative times for each video" do
-      videos = [
-        {video: one_minute_ago, content: /1 minute ago/},
-        {video: two_minutes_ago, content: /2 minutes ago/},
-        {video: one_hour_ago, content: /1 hour ago/},
-        {video: two_hours_ago, content: /2 hours ago/},
-        {video: one_day_ago, content: /yesterday/},
-        {video: two_days_ago, content: /2 days ago/},
-        {video: one_week_ago, content: /7 days ago/},
-        {video: two_weeks_ago, content: /14 days ago/},
-        {video: one_month_ago, content: /last month/},
-        {video: two_months_ago, content: /2 months ago/},
-        {video: one_year_ago, content: /12 months ago/},
-        {video: two_years_ago, content: /2 years ago/},
-      ]
-
-      visit(root_path)
-
-      videos.each do |hash|
-        video_card = find("##{dom_id(hash[:video])}")
-
-        expect(video_card).to have_content(hash[:content])
-      end
-    end
-  end
 end
