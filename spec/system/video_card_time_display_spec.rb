f3bd12b2add968f8c74dd1e1dfbbe0e106a34307
ParamagicDev <konnor5456@gmail.com>
2021-04-14 16:53:17 -0400

    fix to_ms

--- spec/system/video_card_time_display_spec.rb
+++ spec/system/video_card_time_display_spec.rb
@@ -9,17 +9,17 @@ describe "Video Card time displays should display as relative" do
   # Timecop and Rails' built in timecop do not affect the browser time, only the server time.
   # As it is, this is more of a visual regression test since the actual times are handled
   # by a web component.
-  let(:five_minutes_ago) { create(:vod_video, started_at_ms: 5.minutes.to_ms, channel: channel) }
-  let(:one_hour_ago) { create(:vod_video, started_at_ms: 1.hour.to_ms, channel: channel) }
-  let(:two_hours_ago) { create(:vod_video, started_at_ms: 2.hours.to_ms, channel: channel) }
-  let(:one_day_ago) { create(:vod_video, started_at_ms: 1.day.to_ms, channel: channel) }
-  let(:two_days_ago) { create(:vod_video, started_at_ms: 2.days.to_ms, channel: channel) }
-  let(:one_week_ago) { create(:vod_video, started_at_ms: 1.week.to_ms, channel: channel) }
-  let(:two_weeks_ago) { create(:vod_video, started_at_ms: 2.weeks.to_ms, channel: channel) }
-  let(:one_month_ago) { create(:vod_video, started_at_ms: 1.month.to_ms, channel: channel) }
-  let(:two_months_ago) { create(:vod_video, started_at_ms: 2.months.to_ms, channel: channel) }
-  let(:one_year_ago) { create(:vod_video, started_at_ms: 12.months.to_ms, channel: channel) }
-  let(:two_years_ago) { create(:vod_video, started_at_ms: 2.years.to_ms, channel: channel) }
+  let(:five_minutes_ago) { create(:vod_video, started_at_ms: 5.minutes.ago.to_ms, channel: channel) }
+  let(:one_hour_ago) { create(:vod_video, started_at_ms: 1.hour.ago.to_ms, channel: channel) }
+  let(:two_hours_ago) { create(:vod_video, started_at_ms: 2.hours.ago.to_ms, channel: channel) }
+  let(:one_day_ago) { create(:vod_video, started_at_ms: 1.day.ago.to_ms, channel: channel) }
+  let(:two_days_ago) { create(:vod_video, started_at_ms: 2.days.ago.to_ms, channel: channel) }
+  let(:one_week_ago) { create(:vod_video, started_at_ms: 1.week.ago.to_ms, channel: channel) }
+  let(:two_weeks_ago) { create(:vod_video, started_at_ms: 2.weeks.ago.to_ms, channel: channel) }
+  let(:one_month_ago) { create(:vod_video, started_at_ms: 1.month.ago.to_ms, channel: channel) }
+  let(:two_months_ago) { create(:vod_video, started_at_ms: 2.months.ago.to_ms, channel: channel) }
+  let(:one_year_ago) { create(:vod_video, started_at_ms: 12.months.ago.to_ms, channel: channel) }
+  let(:two_years_ago) { create(:vod_video, started_at_ms: 2.years.ago.to_ms, channel: channel) }
 
   let(:videos) {
     [
