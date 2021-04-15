f64739e236f4871e4905e3c3aaf5ae2f62a970c0
ParamagicDev <konnor5456@gmail.com>
2021-04-15 14:36:40 -0400

    change back to unfreeze??

--- spec/models/user_joined_event_spec.rb
+++ spec/models/user_joined_event_spec.rb
@@ -35,7 +35,7 @@ RSpec.describe UserJoinedEvent, type: :model do
     expect(video.user_joined_events.count).to eq(max_per_minute + 1)
 
     # All done!
-    Timecop.return
+    Timecop.unfreeze
   end
 
   def create_new_video_view
