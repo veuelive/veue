3c54d1f437a824c4575cf05f952fe8dcecda420b
Sirbuland <sirbuland.atlas@gmail.com>
2021-04-27 13:46:26 +0500

    [VEUE-677] fix rubocop

--- spec/factories/channels.rb
+++ spec/factories/channels.rb
@@ -3,8 +3,8 @@
 FactoryBot.define do
   factory :channel do
     name { Faker::Name.name[0..15].strip }
-    after (:create) do |channel|
-    	channel.users << create(:user_with_profile)
+    after(:create) do |channel|
+      channel.users << create(:user_with_profile)
     end
   end
 end
