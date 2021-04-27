39e73db11a454cf9667cdd3bd997429380627483
Sirbuland <sirbuland.atlas@gmail.com>
2021-04-27 13:25:07 +0500

    [VEUE-677] fix specs

--- spec/factories/channels.rb
+++ spec/factories/channels.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :channel do
     name { Faker::Name.name[0..15].strip }
     after (:create) do |channel|
-    	channel.users << create(:user)
+    	channel.users << create(:user_with_profile)
     end
   end
 end
