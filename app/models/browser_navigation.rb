f617bc7997325775c8901bf98bf3e4bd079ce541
Sirbuland <sirbuland.atlas@gmail.com>
2021-04-28 05:44:57 +0500

    [VEUE-697] put browser navigation model back

---
+++ app/models/browser_navigation.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+class BrowserNavigation < VideoEvent
+  def input_to_payload
+    {
+      url: input["url"],
+      canGoBack: input["canGoBack"],
+      canGoForward: input["canGoForward"],
+      isLoading: input["isLoading"],
+    }
+  end
+
+  def input_schema
+    {
+      properties: {
+        url: String,
+        canGoBack: :boolean,
+        canGoForward: :boolean,
+        isLoading: :boolean,
+      },
+      required: ["url"],
+    }
+  end
+
+  def self.create_first_navigation!(video, url)
+    video.browser_navigations.create!(
+      timecode_ms: 0,
+      user_id: video.user_id,
+      input: {
+        url: url,
+        canGoBack: false,
+        canGoForward: false,
+        isLoading: false,
+      },
+    )
+  end
+end
