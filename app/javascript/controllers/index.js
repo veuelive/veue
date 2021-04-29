cd0786e61a4e39319ed2248afa82d612f7c625cd
Hampton Catlin <hcatlin@gmail.com>
2021-04-29 17:22:06 -0400

    remove appsignal

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,10 +3,7 @@
 
 import { Application } from "stimulus";
 import { definitionsFromContext } from "stimulus/webpack-helpers";
-import appsignal from "../helpers/integrations/appsignal";
-import { installErrorHandler } from "@appsignal/stimulus";
-
 const application = Application.start();
-installErrorHandler(appsignal, application);
+
 const context = require.context("controllers", true, /_controller\.tsx?$/);
 application.load(definitionsFromContext(context));
