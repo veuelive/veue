a1e9456aead6d6196365748735fa0539e02e177e
Sirbuland <sirbuland.atlas@gmail.com>
2021-05-12 22:43:09 +0500

    [VEUE-772] test with exact driver

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -25,14 +25,15 @@ rescue ActiveRecord::PendingMigrationError => e
   exit(1)
 end
 
-# Return release urls of webdrivers used in selenium
-driver_urls = Webdrivers::Common.subclasses.map { |driver| driver.public_send(:base_url) }
-
 # https://thoughtbot.com/blog/how-to-stub-external-services-in-tests
 require "webmock/rspec"
 WebMock.disable_net_connect!(
   allow_localhost: true,
-  allow: [*driver_urls, Regexp.new(GripBroadcaster.base_url)],
+  allow: [
+    %(https://chromedriver.storage.googleapis.com),
+    %(https://github.com/mozilla/geckodriver/releases/latest),
+    Regexp.new(GripBroadcaster.base_url),
+  ],
 )
 
 RSpec.configure do |config|
