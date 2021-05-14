05eb2e08ed8c523b7256a2dbc4c7771be5f4cc19
ParamagicDev <konnor5456@gmail.com>
2021-05-14 17:23:52 -0400

    remove unneeded comments

--- app/models/discover_curation.rb
+++ app/models/discover_curation.rb
@@ -26,10 +26,6 @@ class DiscoverCuration
 
   # Everything is wrapped in a proc so we dont invoke them until we're ready
   def queries
-    # @live_videos = public_videos.live.limit(3).decorate
-    # @recent_videos = public_videos.finished.decorate
-    # @popular_channels = Channel.most_popular.limit(8).decorate
-    # @upcoming_videos = public_videos.scheduled.order("scheduled_at asc").limit(12).decorate
     @queries ||= {
       trending_this_week: trending_this_week,
       trending_this_month: trending_this_month,
