a156b43049f9b79f53d45e282773374674c288a7
Sirbuland <sirbuland.atlas@gmail.com>
2021-05-15 07:29:20 +0500

    [VEUE-772] fix webmock

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,8 +30,9 @@ require "webmock/rspec"
 WebMock.disable_net_connect!(
   allow_localhost: true,
   allow: [
-    %(https://chromedriver.storage.googleapis.com),
-    %(https://github.com/mozilla/geckodriver),
+    /chromedriver.storage.googleapis.com/,
+    /github.com/,
+    /github-releases.githubusercontent.com/,
     Regexp.new(GripBroadcaster.base_url),
   ],
 )
--- spec/system/broadcast/browser_startup_spec.rb
+++ spec/system/broadcast/browser_startup_spec.rb
@@ -4,11 +4,11 @@ require "system_helper"
 require "support/audience_spec_helpers"
 
 describe "Browser Broadcaster Startup Flow" do
-  describe "browser startup" do
-    before :example do
-      driven_by(:selenium, using: :headless_firefox)
-    end
+  before :example do
+    driven_by(:selenium, using: :headless_firefox)
+  end
 
+  describe "browser startup" do
     it "should display warning if not on chrome" do
       visit "/broadcasts/startup"
       expect(page).to have_content(I18n.t("discover.browser_warning"))
