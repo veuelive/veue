9b18178b337bcdfe2c66974551ad3b41e1b8d4b1
ParamagicDev <konnor5456@gmail.com>
2021-05-19 15:57:37 -0400

    [VEUE-782]: Change how images get attached
    rubocop fixes
    
    remove video spec tests, theyre now tested in by a job / system spec.

---
+++ app/jobs/attach_initial_snapshots_job.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class AttachInitialSnapshotsJob < ApplicationJob
+  queue_as :default
+
+  def perform(video)
+    snapshots = video.video_snapshots.past_snapshots(25)
+
+    video.attach_initial_shots!(snapshots)
+  end
+end
