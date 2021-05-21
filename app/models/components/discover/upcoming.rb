f135c31ceb4806ce2b72776a1ae3a0e8fb3f0cea
ParamagicDev <konnor5456@gmail.com>
2021-05-21 13:47:49 -0400

    [VEUE-785]: Add upcoming stream component
    working on upcoming
    
    fix specS
    
    working on upcoming
    
    finish up testing upcoming
    
    fix system specs
    
    fix minor things
    
    remove param
    
    cache the content

---
+++ app/models/components/discover/upcoming.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module Components
+  module Discover
+    class Upcoming
+      include DiscoverableComponent
+
+      def initialize(component)
+        @component = component
+        @fields = @component.fields
+        @limit = @fields.max_size || 8
+      end
+
+      def to_partial_path
+        "discover/components/upcoming"
+      end
+
+      def title
+        @fields.title
+      end
+
+      def collection
+        return map_channel_names_to_channels if @fields.type == "static_upcoming"
+
+        find_channels
+      end
+
+      def find_channels
+        Channel.most_popular.where.not(next_show_at: nil).limit(@limit).decorate
+      end
+
+      def map_channel_names_to_channels
+        upcoming_slugs = @fields.upcoming_broadcasts.map(&:slug)
+
+        Channel.where(slug: upcoming_slugs).where.not(next_show_at: nil).decorate
+      end
+    end
+  end
+end
--- spec/factories/channels.rb
+++ spec/factories/channels.rb
@@ -3,8 +3,13 @@
 FactoryBot.define do
   factory :channel do
     name { Faker::Name.name[0..15].strip }
-    after(:create) do |channel|
+    transient do
+      next_show_at { nil }
+    end
+
+    after(:create) do |channel, evaluator|
       channel.users << create(:user_with_profile)
+      channel.update_columns(next_show_at: evaluator.next_show_at) if evaluator.next_show_at
     end
   end
 end
