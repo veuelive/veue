884538a97e7badcfd904f5bb0b2c5fd3ba959150
ParamagicDev <konnor5456@gmail.com>
2021-06-08 16:33:18 -0400

    [VEUE-935]: working on channels component

--- app/controllers/discover_controller.rb
+++ app/controllers/discover_controller.rb
@@ -27,6 +27,9 @@ class DiscoverController < ApplicationController
     components.map do |component|
       case component.type
       when "hero_image", "responsive_hero"
+        # hero_image should be removed / replaced with responsive_hero.
+        # responsive_hero is a superset of hero_image and uses ResponsiveHero::Component.new
+        # This exists for backwards compatitibiltiy.
         Components::Discover::Hero.new(component)
       when "content"
         Components::Discover::Content.new(component)
@@ -34,6 +37,8 @@ class DiscoverController < ApplicationController
         Components::Discover::Curation.new(component)
       when "static_upcoming", "dynamic_upcoming"
         Components::Discover::Upcoming.new(component)
+      when "static_channels", "dynamic_channels"
+        DiscoverChannels::Component.new(title: component.fields.title, channels: component.fields.channels)
       end
     end
   end
