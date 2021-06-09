4f57a11519c01c41be21dd17b95d27a106048aff
ParamagicDev <konnor5456@gmail.com>
2021-06-09 14:34:55 -0400

    rubocop all the things

--- app/models/cms_data_mapper.rb
+++ app/models/cms_data_mapper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # A data mapping class for ButterCMS meant to be inherited from to assign specific properties.
 # When finding fields from a component, use hash based bracket ([]) or .dig accessors so it
 # can be easily tested in model / view component tests instead of System Tests.
