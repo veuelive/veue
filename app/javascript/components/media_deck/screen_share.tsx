3b62ce4557d853ab47a003aa1fe31f0d7cbb613b
Sirbuland <sirbuland.atlas@gmail.com>
2021-06-10 12:53:37 +0500

    [VEUE-864] adds funcationality of attaching second camera

--- app/javascript/components/media_deck/screen_share.tsx
+++ app/javascript/components/media_deck/screen_share.tsx
@@ -1,13 +1,15 @@
 import { Component, h, VNode } from "preact";
 import { ScreenCaptureSource } from "helpers/broadcast/capture_sources/screen";
+import { WebcamCaptureSource } from "helpers/broadcast/capture_sources/webcam";
 import {
   NewCaptureSourceEvent,
   RemoveCaptureSourceEvent,
 } from "helpers/broadcast/capture_source_manager";
 import hiddenIcon from "images/visible-false.svg";
 import EventBus from "event_bus";
+import AttachCamera from "components/media_deck/attach_camera";
 
-type State = { isVisible: boolean; screenCaptureSource: ScreenCaptureSource };
+type State = { isVisible: boolean; screenCaptureSource: ScreenCaptureSource; webcamCaptureSource: WebcamCaptureSource };
 export default class ScreenShare extends Component<unknown, State> {
   private videoTag: HTMLVideoElement;
 
@@ -16,7 +18,36 @@ export default class ScreenShare extends Component<unknown, State> {
     this.state = {
       isVisible: true,
       screenCaptureSource: null,
+      webcamCaptureSource: null,
     };
+  }
+
+  render(): VNode {
+    return (
+      <div class="MediaDeck">
+        <div
+          class="MediaDeck__screen-share"
+          onClick={() => {
+            this.toggleVisibility();
+          }}
+        >
+          {this.state.isVisible ? null : (
+            <div class="MediaDeck__screen-share__hidden-cover">
+              <img src={hiddenIcon} alt="hidden icon" />
+            </div>
+          )}
+          <div class="MediaDeck__screen-share__video">
+            <video
+              data-connected={!!this.state.screenCaptureSource}
+              ref={(videoTag) => {
+                this.videoTag = videoTag;
+              }}
+            />
+          </div>
+        </div>
+        {this.renderControls()}
+      </div>
+    );
   }
 
   renderVisibilityControls(): VNode {
@@ -49,43 +80,49 @@ export default class ScreenShare extends Component<unknown, State> {
     return (
       <div class="MediaDeck__controls">
         {this.renderVisibilityControls()}
-        {this.renderStartStopControls()}
+        {!this.state.webcamCaptureSource ? this.renderStartStopControls() : null}
+        {!this.state.screenCaptureSource ? this.renderAttachCameraControls() : null}
       </div>
     );
   }
 
-  render(): VNode {
+  private renderAttachCameraControls(): VNode {
     return (
-      <div class="MediaDeck">
-        <div
-          class="MediaDeck__screen-share"
-          onClick={() => {
-            this.toggleVisibility();
-          }}
-        >
-          {this.state.isVisible ? null : (
-            <div class="MediaDeck__screen-share__hidden-cover">
-              <img src={hiddenIcon} alt="hidden icon" />
-            </div>
-          )}
-          <div class="MediaDeck__screen-share__video">
-            <video
-              data-connected={!!this.state.screenCaptureSource}
-              ref={(videoTag) => {
-                this.videoTag = videoTag;
-              }}
-            />
-          </div>
-        </div>
-        {this.renderControls()}
-      </div>
+      <AttachCamera
+        startCameraStream={this.addWebcamCaptureSource.bind(this)}
+        stopCameraStream={this.stopWebcamStream.bind(this)}
+      />
     );
+  }
+
+  private async addWebcamCaptureSource(deviceId: string): Promise<void> {
+    const webcamCaptureSource = await WebcamCaptureSource.connect(
+      deviceId,
+      this.videoTag
+    );
+
+    this.setState({
+      webcamCaptureSource
+    });
+
+    EventBus.dispatch(
+      NewCaptureSourceEvent,
+      webcamCaptureSource
+    );
+  }
+
+  private stopWebcamStream(): void {
+    EventBus.dispatch(RemoveCaptureSourceEvent, this.state.webcamCaptureSource);
+    this.state.webcamCaptureSource.stop();
+    this.videoTag.srcObject = null;
+    this.setState({ webcamCaptureSource: null });
   }
 
   private async addScreenCaptureSource() {
     const screenCaptureSource = await ScreenCaptureSource.connect(
       this.videoTag
     );
+
     this.setState({
       isVisible: false,
       screenCaptureSource,
