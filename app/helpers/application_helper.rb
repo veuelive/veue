4ec05fee96c6532614c3e4c1c98c4897704c1e48
ParamagicDev <konnor5456@gmail.com>
2021-06-14 12:58:42 -0400

    [VEUE-937]: Add a /videos routes and grid fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  include Pagy::Frontend
+
   def body_attributes
     {
       id: "#{controller.controller_name}__#{controller.action_name}",
